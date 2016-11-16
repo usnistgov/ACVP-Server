16d1861c9623c378c79ae445e4ad3480ed61f137
Dean Fiala <dean.fiala@g2-inc.com>
2016-11-16 19:12:29 +0000

    Added Rijndael class for testing underlying methods in the base class
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@385678 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ aes-gcm/src/NIST.CVP.Generation.AES/RijndaelTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIST.CVP.Generation.AES
+{
+    public class RijndaelTest : Rijndael
+    {
+        protected override void BlockEncryptWorker(Cipher cipher, Key key, byte[] plainText, int numBlocks, byte[,] block, byte[] outBuffer)
+        {
+            throw new NotImplementedException("This class is purely here as a testing facade for the underlying Rijndael implementation methods. It should never be used for encryption");
+        }
+
+        public new void KeyAddition(byte[,] block, byte[,] roundKey, int blockCount)
+        {
+            base.KeyAddition(block, roundKey, blockCount);
+        }
+
+        public new void Substitution(byte[,] block, byte[] box, int blockCount)
+        {
+            base.Substitution(block, box, blockCount);
+        }
+
+        public new void ShiftRow(byte[,] block, int d, int blockCount)
+        {
+            base.ShiftRow(block, d, blockCount);
+        }
+
+        public new void MixColumn(byte[,] block, int blockCount)
+        {
+            base.MixColumn(block,  blockCount);
+        }
+
+        public new void Multiply(byte a, byte b)
+        {
+            base.Multiply(a, b);
+        }
+    }
+}
