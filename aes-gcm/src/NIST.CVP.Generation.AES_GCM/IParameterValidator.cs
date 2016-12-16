60d863a12d8eaba96a9d8f4645777aca8da85652
Dean Fiala <dean.fiala@g2-inc.com>
2016-12-16 15:13:08 +0000

    Lost IParameterValidator somehow
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386048 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ aes-gcm/src/NIST.CVP.Generation.AES_GCM/IParameterValidator.cs
@@ -0,0 +1,9 @@
+﻿using NIST.CVP.Generation.Core;
+
+namespace NIST.CVP.Generation.AES_GCM
+{
+    public interface IParameterValidator
+    {
+        ParameterValidateResponse Validate(Parameters parameters);
+    }
+}
