1e74d7e57f5e3ea92ead3d4bf229355e9db60921
Christopher Celi <christopher.celi@nist.gov>
2016-12-20 20:57:21 +0000

    Adds ConfigParser to NIST.CVP.Generation.Core so configuration settings can be read from JSON files. Includes Unit Tests for the new class.
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386087 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ common/src/NIST.CVP.Generation.Core/ConfigParser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NIST.CVP.Generation.Core
+{
+    public class ConfigParser
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public bool success { get; set; }
+
+        public ConfigParser(string path)
+        {
+
+            if(string.IsNullOrEmpty(path) || !File.Exists(path))
+            {
+                success = false;
+                Configuration = null;
+            }else
+            {
+                try
+                {
+                    // Put the success flag first so when it fails we know it is changed
+                    success = true;
+                    var configBuilder = new ConfigurationBuilder().AddJsonFile(path);
+                    Configuration = configBuilder.Build();
+
+                } catch
+                {
+                    success = false;
+                    Configuration = null;
+                }
+            }
+        }
+    }
+}
---
+++ common/test/NIST.CVP.Generation.Core.Tests/ConfigParserTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using NIST.CVP.Generation.Core;
+using NIST.CVP.Tests.Core;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace NIST.CVP.Generation.Core.Tests
+{
+
+    [TestFixture]
+    public class ConfigParserTests
+    {
+        private string _unitTestPath;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _unitTestPath = Utilities.GetConsistentTestingStartPath(GetType(), @"..\..\TestFiles\ConfigFiles\");
+        }
+
+        [Test]
+        public void ShouldReturnErrorForNonExistentFile()
+        {
+            string path = Path.Combine(_unitTestPath, "badPath.json");
+            ConfigParser configParser = new ConfigParser(path);
+            Assert.IsNotNull(configParser.success);
+            Assert.IsFalse(configParser.success);
+        }
+
+        [Test]
+        public void ShouldReturnErrorForNonExistentPath()
+        {
+            string path = $@"C:\{Guid.NewGuid()}\exampleConfig.json";
+            ConfigParser configParser = new ConfigParser(path);
+            Assert.IsNotNull(configParser.success);
+            Assert.IsFalse(configParser.success);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void ShouldReturnErrorForNullOrEmptyPath(string path)
+        {
+            ConfigParser configParser = new ConfigParser(path);
+            Assert.IsNotNull(configParser.success);
+            Assert.IsFalse(configParser.success);
+        }
+
+        [Test]
+        public void ShouldParseValidFile()
+        {
+            string path = Path.Combine(_unitTestPath, "exampleConfig.json");
+            ConfigParser configParser = new ConfigParser(path);
+            Assert.IsNotNull(configParser.success);
+            Assert.IsTrue(configParser.success);
+        }
+
+        [Test]
+        public void ShouldReadCorrectValuesFromFields()
+        {
+            string path = Path.Combine(_unitTestPath, "exampleConfig.json");
+            ConfigParser configParser = new ConfigParser(path);
+            Assume.That(configParser.success);
+            Assert.AreEqual(10, int.Parse(configParser.Configuration["testField"]));
+            Assert.AreEqual(20, int.Parse(configParser.Configuration["secondField"]));
+        }
+
+        [Test]
+        public void ShouldReadCorrectValuesFromArrays()
+        {
+            string path = Path.Combine(_unitTestPath, "exampleConfig.json");
+            ConfigParser configParser = new ConfigParser(path);
+            Assume.That(configParser.success);
+            Assert.AreEqual(30, int.Parse(configParser.Configuration["arrayField:0"]));
+            Assert.AreEqual(40, int.Parse(configParser.Configuration["arrayField:1"]));
+        }
+
+        [Test]
+        public void ShouldReadCorrectValuesFromObjects()
+        {
+            string path = Path.Combine(_unitTestPath, "exampleConfig.json");
+            ConfigParser configParser = new ConfigParser(path);
+            Assume.That(configParser.success);
+            Assert.AreEqual(50, int.Parse(configParser.Configuration["objectField:insideField"]));
+        }
+    }
+}
