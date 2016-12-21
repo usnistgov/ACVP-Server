0a61184f4e3149a36ae9acfc2328e30f2196c956
Christopher Celi <christopher.celi@nist.gov>
2016-12-21 21:12:25 +0000

    Begins adding AlgoFileFinderBaseTests along with TestFiles and FakeAlgoFileFinderBase
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386114 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ common/test/NIST.CVP.Generation.Core.Tests/AlgoFileFinderBaseTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NIST.CVP.Generation.Core.Tests.Fakes;
+using NIST.CVP.Tests.Core;
+using NUnit.Framework;
+
+namespace NIST.CVP.Generation.Core.Tests
+{
+    [TestFixture]
+    public class AlgoFileFinderBaseTests
+    {
+
+        // TODO: Do actual file pathing for everything
+        // TODO: Make sure tests reflect purpose of the class
+
+        private string _testPath;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _testPath = Utilities.GetConsistentTestingStartPath(GetType(), @"..\..\TestFiles\algoFileFinderBaseTests");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Directory.Delete(_testPath, true);
+        }
+
+        [Test]
+        [TestCase(@"some/file/path/")]
+        [TestCase(null)]
+        [TestCase("")]
+        public void ShouldCorrectlyHandleAllFilePrefixes(string filePrefix)
+        {
+            var subject = GetSubject(filePrefix);
+            Assume.That(subject.Name == "FakeAlgo");
+            Assert.IsTrue(subject.FilePrefix == filePrefix);
+        }
+
+        [Test]
+        public void UseFilePrefixShouldBeTrueForNonNullOrNonEmptyFilePrefix()
+        {
+            var subject = GetSubject(@"some/file/path/");
+            Assert.IsTrue(subject.UseFilePrefix);
+        }
+
+        [Test]
+        public void ShouldNotCopyFilesIfSourceDirectoryDoesNotExist()
+        {
+            var subject = GetSubject(@"some/file/path/");
+            var result = subject.CopyFoundFilesToTargetDirectory(@"file/path/that/does/not/exist/", "fileFolderName",
+                "targetDirectory");
+            Assert.AreEqual(-1, result);
+        }
+
+        [Test]
+        public void ExtractFilesFromZipShouldBeTrueWhenValid()
+        {
+            var subject = GetSubject(null);
+            var result = subject.ExtractFilesFromZip("filepath", "extractionpath");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase(@"source/does/not/exist/", "extractionpath")]
+        [TestCase("sourcepath", @"destination/does/not/exist/")]
+        [TestCase(@"source/does/not/exist/", @"destination/does/not/exist")]
+        public void ExtractFilesFromZipShouldBeFalseWhenInvalid(string sourcePath, string destinationPath)
+        {
+            var subject = GetSubject(null);
+            var result = subject.ExtractFilesFromZip(sourcePath, destinationPath);
+            Assert.IsFalse(result);
+        }
+        
+        private FakeAlgoFileFinderBase GetSubject(string filePrefix)
+        {
+            string path = Path.Combine(_testPath, filePrefix);
+            return new FakeAlgoFileFinderBase("FakeAlgo", path);
+        }
+    }
+}
---
+++ common/test/NIST.CVP.Generation.Core.Tests/Fakes/FakeAlgoFileFinderBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIST.CVP.Generation.Core.Tests.Fakes
+{
+    public class FakeAlgoFileFinderBase : AlgoFileFinderBase
+    {
+        public override string Name { get; }
+        public override string FilePrefix { get; }
+
+        public FakeAlgoFileFinderBase(string algoName, string filePrefix)
+        {
+            Name = algoName;
+            FilePrefix = filePrefix;
+        }
+    }
+}
