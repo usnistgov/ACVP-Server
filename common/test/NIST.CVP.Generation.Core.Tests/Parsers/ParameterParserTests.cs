e29babb9b7912b671824b2dc0eedfec04074c442
Christopher Celi <christopher.celi@nist.gov>
2016-12-23 17:02:03 +0000

    Adds tests to Parsers/ParameterParser.cs
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386142 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ common/test/NIST.CVP.Generation.Core.Tests/Parsers/ParameterParserTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NIST.CVP.Generation.Core.Parsers;
+using NIST.CVP.Tests.Core;
+using NUnit.Framework;
+
+namespace NIST.CVP.Generation.Core.Tests.Parsers
+{
+    public class ParameterParserTests
+    {
+        private string _unitTestPath;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _unitTestPath = Utilities.GetConsistentTestingStartPath(GetType(), @"..\..\TestFiles");
+        }
+
+        [Test]
+        public void ShouldReturnErrorForNonExistentPath()
+        {
+            var subject = new ParameterParser<IParameters>();
+            var result = subject.Parse(@"path/does/not/exist/file.json");
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void ShouldReturnErrorForNullOrEmptyPath(string path)
+        {
+            var subject = new ParameterParser<IParameters>();
+            var result = subject.Parse(path);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+
+        [Test]
+        public void ShouldReturnErrorForBadFile()
+        {
+            var subject = new ParameterParser<IParameters>();
+            var path = Path.Combine(_unitTestPath, "notJson.json");
+            var result = subject.Parse(path);
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Success);
+        }
+    }
+}
