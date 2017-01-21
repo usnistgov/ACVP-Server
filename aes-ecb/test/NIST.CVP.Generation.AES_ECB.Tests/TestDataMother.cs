8ed34ee48abbed736bca54866b00d7ec4cee0e10
Russell Hammett <russell.hammett@nist.gov>
2017-01-21 11:49:22 +0000

    Additional TestVectorSet tests related to MCT
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386373 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- aes-ecb/test/NIST.CVP.Generation.AES_ECB.Tests/TestDataMother.cs
+++ aes-ecb/test/NIST.CVP.Generation.AES_ECB.Tests/TestDataMother.cs
@@ -42,5 +42,46 @@ namespace NIST.CVP.Generation.AES_ECB.Tests
             }
             return testGroups;
         }
+
+        public List<TestGroup> GetMCTTestGroups(int groups = 1, string direction = "encrypt")
+        {
+            List<TestGroup> testGroups = new List<TestGroup>();
+
+            for (int groupIdx = 0; groupIdx < groups; groupIdx++)
+            {
+
+                var tests = new List<ITestCase>();
+                for (int testId = 15 * groupIdx + 1; testId <= (groupIdx + 1) * 15; testId++)
+                {
+                    tests.Add(new TestCase
+                    {
+                        Deferred = false,
+                        ResultsArray = new List<AlgoArrayResponse>()
+                        {
+                            new AlgoArrayResponse()
+                            {
+                                PlainText = new BitString("1AAADFFF"),
+                                CipherText = new BitString("7EADDC"),
+                                Key = new BitString("9998ADCD"),
+                            }
+                        },
+                        TestCaseId = testId
+                    });
+                }
+
+                testGroups.Add(
+                    new TestGroup
+                    {
+
+                        Function = direction,
+                        KeyLength = 256 + groupIdx * 2,
+                        Tests = tests,
+                        TestType = "MCT"
+                    }
+                );
+            }
+
+            return testGroups;
+        }
     }
 }
