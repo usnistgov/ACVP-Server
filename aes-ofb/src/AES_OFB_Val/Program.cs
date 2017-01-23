3ff3a09cf7c2e5dd7306f8e744d5a2c240675a27
Russell Hammett <russell.hammett@nist.gov>
2017-01-23 13:18:31 +0000

    Solution and project skeleton for AES-OFB
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386379 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ aes-ofb/src/AES_OFB_Val/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AES_OFB_Val
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
