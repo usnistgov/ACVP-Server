603b61b10940d397fc1022733a3340e0ab2ca7a9
Russell Hammett <russell.hammett@nist.gov>
2017-02-04 14:20:59 +0000

    CFB1 - Decrypt.  Passing MMTs.
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386590 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- aes-cfb1/test/NIST.CVP.Generation.AES_CFB1.IntegrationTests/MMTs.cs
+++ aes-cfb1/test/NIST.CVP.Generation.AES_CFB1.IntegrationTests/MMTs.cs
@@ -13,6 +13,7 @@ namespace NIST.CVP.Generation.AES_CFB1.IntegrationTests
     {
         private AES_CFB1 _subject =new AES_CFB1(new RijndaelFactory(new RijndaelInternals()));
 
+        #region Encrypt
         [Test]
         public void ShouldMMTEncrypt1Byte128BitKey()
         {
@@ -90,31 +91,86 @@ namespace NIST.CVP.Generation.AES_CFB1.IntegrationTests
 
             Assert.AreEqual(expectedCipherText, result.CipherText);
         }
+        #endregion Encrypt
 
-        //[Test]
-        //public void ShouldMMTDecrypt1Byte()
-        //{
-        //    BitString key = new BitString("83185129c67311b6fc765639b7bb63c0");
-        //    BitString iv = new BitString("84324d79ab6adc655d295ee9f8263725");
-        //    BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("10");
-        //    BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("01");
+        #region Decrypt
+        [Test]
+        public void ShouldMMTDecrypt1Byte128BitKey()
+        {
+            BitString key = new BitString("83185129c67311b6fc765639b7bb63c0");
+            BitString iv = new BitString("84324d79ab6adc655d295ee9f8263725");
+            BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("10");
+            BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("01");
 
-        //    var result = _subject.BlockDecrypt(iv, key, cipherText);
+            var result = _subject.BlockDecrypt(iv, key, cipherText);
 
-        //    Assert.AreEqual(expectedPlainText, result.PlainText);
-        //}
+            Assert.AreEqual(expectedPlainText, result.PlainText);
+        }
 
-        //[Test]
-        //public void ShouldMMTDecryptMultipleBytes()
-        //{
-        //    BitString key = new BitString("2a5e39f2de044223a0fcde0327331602");
-        //    BitString iv = new BitString("5e343033f92a3c82efdf9a35ac5e3657");
-        //    BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("1001110000");
-        //    BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("0000011011");
+        [Test]
+        public void ShouldMMTDecryptMultipleBytes128BitKey()
+        {
+            BitString key = new BitString("2a5e39f2de044223a0fcde0327331602");
+            BitString iv = new BitString("5e343033f92a3c82efdf9a35ac5e3657");
+            BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("1001110000");
+            BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("0000011011");
 
-        //    var result = _subject.BlockDecrypt(iv, key, cipherText);
+            var result = _subject.BlockDecrypt(iv, key, cipherText);
 
-        //    Assert.AreEqual(expectedPlainText, result.PlainText);
-        //}
+            Assert.AreEqual(expectedPlainText, result.PlainText);
+        }
+
+        [Test]
+        public void ShouldMMTDecrypt1Byte192BitKey()
+        {
+            BitString key = new BitString("6ec4710e35f8794eb50109d8a4c63fe8a672691c28c71171");
+            BitString iv = new BitString("6e63d7b3e89e6d5800827a492c40ba5b");
+            BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("10");
+            BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("10");
+
+            var result = _subject.BlockDecrypt(iv, key, cipherText);
+
+            Assert.AreEqual(expectedPlainText, result.PlainText);
+        }
+
+        [Test]
+        public void ShouldMMTDecryptMultipleBytes192BitKey()
+        {
+            BitString key = new BitString("677bb41a620fae519723937ebfe1f7b430970056505d76db");
+            BitString iv = new BitString("be91c031133b08c35ca153ad7cecb110");
+            BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("1000110010");
+            BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("1100100011");
+
+            var result = _subject.BlockDecrypt(iv, key, cipherText);
+
+            Assert.AreEqual(expectedPlainText, result.PlainText);
+        }
+
+        [Test]
+        public void ShouldMMTDecrypt1Byte256BitKey()
+        {
+            BitString key = new BitString("9992afb14c86eaf1f2a2aea3276193c8796abad31d6f18b0e1551104629549b7");
+            BitString iv = new BitString("8a8f6521669c93d3d5fea9237b929f70");
+            BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("00");
+            BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("00");
+
+            var result = _subject.BlockDecrypt(iv, key, cipherText);
+
+            Assert.AreEqual(expectedPlainText, result.PlainText);
+        }
+
+        [Test]
+        public void ShouldMMTDecryptMultipleBytes256BitKey()
+        {
+            BitString key = new BitString("274e1639d0b7ecc24c3ea8d968092be8b2fe0f313c7b8d1a9c479dc737c95eee");
+            BitString iv = new BitString("bd8831473e62ce2f92873d8ac4677b77");
+            BitString cipherText = BitString.GetBitStringEachCharacterOfInputIsBit("1111101010");
+            BitString expectedPlainText = BitString.GetBitStringEachCharacterOfInputIsBit("0101100011");
+
+            var result = _subject.BlockDecrypt(iv, key, cipherText);
+
+            Assert.AreEqual(expectedPlainText, result.PlainText);
+        }
+        #endregion Decrypt
     }
 }
