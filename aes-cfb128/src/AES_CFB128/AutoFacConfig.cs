4db5348450fa1577afadba9cfe9b1450a98bc49e
Russell Hammett <russell.hammett@nist.gov>
2017-02-15 19:16:37 +0000

    AES-CFB128 Gen/Val, integration tests, FireHoseTests
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386791 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ aes-cfb128/src/AES_CFB128/AutoFacConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Autofac;
+using NIST.CVP.Generation.AES;
+using NIST.CVP.Generation.AES_CFB128;
+using NIST.CVP.Generation.Core.Parsers;
+using NIST.CVP.Math;
+
+namespace AES_CFB128
+{
+    public class AutofacConfig
+    {
+        private static IContainer _container;
+
+        public static Action<ContainerBuilder> OverrideRegistrations;
+
+        public static IContainer Container
+        {
+            get { return _container; }
+        }
+        public static void IoCConfiguration()
+        {
+            var builder = new ContainerBuilder();
+
+
+            builder.RegisterType<Generator<Parameters, TestVectorSet>>();
+            builder.RegisterType<NIST.CVP.Generation.AES_CFB128.AES_CFB128>().AsImplementedInterfaces();
+            builder.RegisterType<AES_CFB128_MCT>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactoryFactory>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<KnownAnswerTestCaseGeneratorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<TestVectorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<KATTestGroupFactory>().AsImplementedInterfaces();
+            builder.RegisterType<MCTTestGroupFactory>().AsImplementedInterfaces();
+            builder.RegisterType<ParameterValidator>().AsImplementedInterfaces();
+            builder.RegisterType<ParameterParser<Parameters>>().AsImplementedInterfaces();
+            builder.RegisterType<Random800_90>().AsImplementedInterfaces();
+            builder.RegisterType<RijndaelInternals>().AsImplementedInterfaces();
+            builder.RegisterType<RijndaelFactory>().AsImplementedInterfaces();
+
+            OverrideRegistrations?.Invoke(builder);
+
+            _container = builder.Build();
+
+        }
+    }
+}
--- aes-cfb128/src/AES_CFB128/Program.cs
+++ aes-cfb128/src/AES_CFB128/Program.cs
@@ -1,14 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using Autofac;
+using NIST.CVP.Generation.AES;
+using NIST.CVP.Generation.AES_CFB128;
+using NIST.CVP.Generation.Core;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace AES_CFB128
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("No arguments supplied");
+                return 1;
+            }
+            var requestFile = args[0];
+            LoggingHelper.ConfigureLogging(requestFile, "aes-cfb8");
+            Logger.Info($"Generating Test Vectors for {requestFile}");
+
+            //get generator and call it
+            Logger.Debug("Generating");
+
+            try
+            {
+                AutofacConfig.IoCConfiguration();
+                using (var scope = AutofacConfig.Container.BeginLifetimeScope())
+                {
+                    var gen = scope.Resolve<Generator<Parameters,TestVectorSet>>();
+                    var result = gen.Generate(requestFile);
+                    if (!result.Success)
+                    {
+                        Console.Error.WriteLine(
+                            $"ERROR! Generating Test Vectors for {requestFile}: {result.ErrorMessage}");
+                        Logger.Error($"ERROR! Generating Test Vectors for {requestFile}: {result.ErrorMessage}");
+                        //Console.ReadLine();
+                        return 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"ERROR! Generating Test Vectors for {requestFile}: {ex.Message}");
+                return 1;
+            }
+
+
+            Logger.Debug($"Success! Generating Test Vectors for {requestFile}");
+            //Console.ReadLine();
+
+            return 0;
         }
+
+        private static Logger Logger
+        {
+            get { return LogManager.GetLogger("Generate"); }
+        }
+
+      
     }
 }
