23f94b26634fb11237951a92fb084acbcc839256
Ryan Horan <ryan.horan@nist.gov>
2017-02-22 18:36:04 +0000

    Created TDES_CBC_VAL and made small change to TestVectorFactory
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@386910 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ tdes-cbc/src/TDES_CBC_Val/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Autofac;
+using Microsoft.Extensions.PlatformAbstractions;
+using NIST.CVP.Generation.Core;
+
+namespace TDES_CBC_Val
+{
+    public class Program
+        {
+            public static int Main(string[] args)
+            {
+                if (args.Length < 3)
+                {
+                    Console.Error.WriteLine("Not enough arguments supplied, must supply paths for result, prompt and and answer files");
+                    return 1;
+                }
+                var resultFile = args[0];
+                var promptFile = args[1];
+                var answerFile = args[2];
+                LoggingHelper.ConfigureLogging(resultFile, "tdes-cbc-val");
+                Logger.Info($"Running {PlatformServices.Default.Application.ApplicationName} { PlatformServices.Default.Application.ApplicationVersion}");
+
+                Logger.Info($"Validating test results for {resultFile}");
+                try
+                {
+                    AutofacConfig.IoCConfiguration();
+                    using (var scope = AutofacConfig.Container.BeginLifetimeScope())
+                    {
+                        var validator = scope.Resolve<NIST.CVP.Generation.TDES_CBC.Validator>();
+                        var result = validator.Validate(resultFile, answerFile, promptFile);
+                        if (!result.Success)
+                        {
+                            Console.Error.WriteLine($"ERROR! Validating Test Vectors for {resultFile}: {result.ErrorMessage}");
+                            Logger.Error($"ERROR! Validating Test Vectors for {resultFile}: {result.ErrorMessage}");
+                            //Console.ReadLine();
+                            return 1;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error($"ERROR! Validating Test Vectors for {resultFile}: {ex.Message}");
+                    return 1;
+                }
+
+                Logger.Info($"Success! Validating Test Results for {resultFile}");
+                //Console.ReadLine();
+
+                return 0;
+            }
+
+            private static Logger Logger
+            {
+                get { return LogManager.GetLogger("Validate"); }
+            }
+
+     }
+}
