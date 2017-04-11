a66a968bac54481ddaa79a4465673e0a5020fa5d
Russell Hammett <russell.hammett@nist.gov>
2017-04-11 17:13:02 +0000

    Project stubs for AES namespace "Crypto" refactor
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@387828 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ aes-gcm/test/NIST.CVP.Crypto.AES_GCM.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NIST.CVP.Crypto.AES_GCM.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32e06643-aa59-4a24-9ce1-4449c6d489ed")]
