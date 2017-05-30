ddb5ee62f9b418df5292dae70fca0a288018d003
Christopher Celi <christopher.celi@nist.gov>
2017-05-30 15:35:17 +0000

    Adds MCTTestGroupFactory.cs for SHA3
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@388750 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ sha3/src/NIST.CVP.Generation.SHA3/MCTTestGroupFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NIST.CVP.Generation.Core;
+
+namespace NIST.CVP.Generation.SHA3
+{
+    public class MCTTestGroupFactory : IMonteCarloTestGroupFactory<Parameters, TestGroup>
+    {
+        public IEnumerable<TestGroup> BuildMCTTestGroups(Parameters parameters)
+        {
+            var testGroups = new List<TestGroup>();
+
+            foreach (var digSize in parameters.DigestSizes)
+            {
+                var testGroup = new TestGroup
+                {
+                    Function = parameters.Algorithm,
+                    DigestSize = digSize,
+                    BitOrientedInput = parameters.BitOrientedInput,
+                    BitOrientedOutput = parameters.BitOrientedOutput,
+                    MinOutputLength = parameters.MinOutputLength,
+                    MaxOutputLength = parameters.MaxOutputLength,
+                    TestType = "MCT"
+                };
+
+                testGroups.Add(testGroup);
+            }
+
+            return testGroups;
+        }
+    }
+}
