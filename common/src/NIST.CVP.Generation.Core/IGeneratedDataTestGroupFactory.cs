61272808bd4cfdaadf2d8a0e03e6de2063ae3cce
Christopher Celi <christopher.celi@nist.gov>
2017-06-12 15:30:39 +0000

    Adds IAFTTestGroupFactory and IGDTTestGroupFactory
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@388945 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ common/src/NIST.CVP.Generation.Core/IGeneratedDataTestGroupFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIST.CVP.Generation.Core
+{
+    public interface IGeneratedDataTestGroupFactory<in TParameters, out TTestGroup>
+        where TParameters : IParameters
+        where TTestGroup : ITestGroup
+    {
+        IEnumerable<TTestGroup> BuildGDTTestGroups(TParameters parameters);
+    }
+}
---
+++ common/test/NIST.CVP.Tests.Core/Fakes/NullAFTTestGroupFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using NIST.CVP.Generation.Core;
+
+namespace NIST.CVP.Tests.Core.Fakes
+{
+    public class NullAFTTestGroupFactory<TParameters, TTestGroup> : IAlgorithmFunctionalTestGroupFactory<TParameters, TTestGroup>
+        where TParameters : IParameters
+        where TTestGroup : ITestGroup
+    {
+        public IEnumerable<TTestGroup> BuildAFTTestGroups(TParameters parameters)
+        {
+            return null;
+        }
+    }
+}
