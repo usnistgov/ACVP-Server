3953546cf7c1c0f36408d58a1c125d8b99095ee9
Christopher Celi <christopher.celi@nist.gov>
2017-07-25 20:45:03 +0000

    Adds some tests to RSA SigGen
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@389705 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ rsa/test/NIST.CVP.Generation.RSA-SigGen.Tests/ParametersTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NIST.CVP.Tests.Core.TestCategoryAttributes;
+
+namespace NIST.CVP.Generation.RSA_SigGen.Tests
+{
+    [TestFixture, UnitTest]
+    public class ParametersTests
+    {
+        [Test]
+        public void ShouldCoverParametersSet()
+        {
+            var parameters = new Parameters
+            {
+                Algorithm = "RSA",
+                Mode = "SigGen",
+                IsSample = false,
+                Moduli = new[] { 2048, 3072, 4096 },
+                HashAlgs = new[] { "SHA-1", "SHA-256" },
+                SigGenModes = new[] { "ANSX9.31", "PKCS1v15", "PSS" },
+                SaltMode = "fixed",
+                Salt = "ABCD",
+                SaltLen = new[] { 20, 20, 20 },
+                N = "ABCD",
+                PrivExp = "ABCD",
+                PubExp = "ABCD"
+            };
+
+            Assert.IsNotNull(parameters);
+        }
+
+        [Test]
+        public void ShouldCoverParametersGet()
+        {
+            var parameters = new Parameters
+            {
+                Algorithm = "RSA",
+                Mode = "SigGen",
+                IsSample = false,
+                Moduli = new[] { 2048, 3072, 4096 },
+                HashAlgs = new[] { "SHA-1", "SHA-256" },
+                SigGenModes = new[] { "ANSX9.31", "PKCS1v15", "PSS" },
+                SaltMode = "fixed",
+                Salt = "ABCD",
+                SaltLen = new[] { 20, 20, 20 },
+                N = "ABCD",
+                PrivExp = "ABCD",
+                PubExp = "ABCD"
+            };
+
+            Assert.AreEqual("RSA", parameters.Algorithm);
+        }
+    }
+}
