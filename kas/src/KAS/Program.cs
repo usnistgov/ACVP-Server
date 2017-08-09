3afee1081df213196bcec20006f0b1b5cd789f8d
Russell Hammett <russell.hammett@nist.gov>
2017-08-09 14:04:41 +0000

    KAS project stubs
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@389917 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ kas/src/KAS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ kas/src/KAS_Val/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KAS_Val
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
