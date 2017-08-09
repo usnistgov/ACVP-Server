1c86b70f3e112be13ad6d7bd956772c1b2a1b046
Christopher Celi <christopher.celi@nist.gov>
2017-08-09 21:13:00 +0000

    Adds exe files to RSA SigGen
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@389937 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ rsa/src/RSA-SigGen_Val/AutofacConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Autofac;
+using NIST.CVP.Generation.Core;
+using NIST.CVP.Generation.Core.Parsers;
+using NIST.CVP.Generation.RSA_SigGen;
+using NIST.CVP.Math;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSA_SigGen_Val
+{
+    public class AutofacConfig
+    {
+        private static IContainer _container;
+        public static Action<ContainerBuilder> OverrideRegistrations;
+        public static IContainer Container { get { return _container; } }
+
+        public static void IoCConfiguration()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Generator<Parameters, TestVectorSet>>().AsImplementedInterfaces();
+            builder.RegisterType<Validator<TestVectorSet, TestCase>>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<TestReconstitutor>().AsImplementedInterfaces();
+            builder.RegisterType<TestVectorFactory<Parameters, TestVectorSet>>().AsImplementedInterfaces();
+            builder.RegisterType<ParameterValidator>().AsImplementedInterfaces();
+            builder.RegisterType<ResultValidator<TestCase>>().AsImplementedInterfaces();
+            builder.RegisterType<ParameterParser<Parameters>>().AsImplementedInterfaces();
+            builder.RegisterType<DynamicParser>().AsImplementedInterfaces();
+            builder.RegisterType<Random800_90>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseValidatorFactory>().AsImplementedInterfaces();
+
+            OverrideRegistrations?.Invoke(builder);
+            _container = builder.Build();
+        }
+    }
+}
