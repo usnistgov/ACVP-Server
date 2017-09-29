5d6d878eff2921c7dabb93ea6266e1b4d77fbe79
Russell Hammett <russell.hammett@nist.gov>
2017-09-29 20:11:03 +0000

    DsaFactory stub for use in KAS
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@390714 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ crypto/src/NIST.CVP.Crypto.DSA.FFC/IDsaFfcFactory.cs
@@ -0,0 +1,10 @@
+﻿using NIST.CVP.Crypto.SHAWrapper;
+using NIST.CVP.Math.Entropy;
+
+namespace NIST.CVP.Crypto.DSA.FFC
+{
+    public interface IDsaFfcFactory
+    {
+        IDsaFfc GetInstance(ISha sha, EntropyProviderTypes entropyType = EntropyProviderTypes.Random);
+    }
+}
