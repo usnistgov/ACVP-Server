13bcf4f6e5d7ed727c0956b025d20d3c0859e797
Russell Hammett <russell.hammett@nist.gov>
2017-10-02 17:00:08 +0000

    Enum helper
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@390731 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ common/src/NIST.CVP.Generation.Core/Helpers/EnumHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+
+namespace NIST.CVP.Generation.Core.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static string GetEnumDescriptionFromEnum(Enum enumToGetDescriptionFrom)
+        {
+            FieldInfo fi = enumToGetDescriptionFrom.GetType().GetField(enumToGetDescriptionFrom.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                    typeof(DescriptionAttribute),
+                    false);
+
+            if (attributes != null &&
+                attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+
+            return enumToGetDescriptionFrom.ToString();
+        }
+    }
+}
---
+++ common/test/NIST.CVP.Generation.Core.Tests/Helpers/EnumHelpersTests.cs
@@ -0,0 +1,30 @@
+﻿using NIST.CVP.Tests.Core.TestCategoryAttributes;
+using NUnit.Framework;
+using System.ComponentModel;
+using NIST.CVP.Generation.Core.Helpers;
+
+namespace NIST.CVP.Generation.Core.Tests.Helpers
+{
+    [TestFixture, UnitTest]
+    public class EnumHelpersTests
+    {
+        private const string TestDescription = "this is different";
+
+        public enum FakeEnum
+        {
+            First,
+            Second,
+            [System.ComponentModel.Description(TestDescription)]
+            Third
+        }
+
+        [Test]
+        [TestCase(FakeEnum.First, "First")]
+        [TestCase(FakeEnum.Second, "Second")]
+        [TestCase(FakeEnum.Third, TestDescription)]
+        public void ShouldReturnCorrectEnumString(FakeEnum value, string expectedValue)
+        {
+            Assert.AreEqual(expectedValue, EnumHelpers.GetEnumDescriptionFromEnum(value));
+        }
+    }
+}
