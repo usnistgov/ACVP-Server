3895344d4b9c2e881ddbd6346b8d20db48735fb6
Russell Hammett <russell.hammett@nist.gov>
2017-11-30 18:04:43 +0000

    CryptoResult interface, obsoleting previous interface
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@391266 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- common/src/NIST.CVP.Generation.Core/ICryptoResult.cs
+++ common/src/NIST.CVP.Generation.Core/ICryptoResult.cs
@@ -1,8 +1,11 @@
-﻿namespace NIST.CVP.Generation.Core
+﻿using System;
+
+namespace NIST.CVP.Generation.Core
 {
     /// <summary>
     /// Represents the result from a crypto algo
     /// </summary>
+    [Obsolete("This should be moved to NIST.CVP.Crypto.Common at earliest convenience")]
     public interface ICryptoResult
     {
     }
---
+++ crypto/src/NIST.CVP.Crypto.Common/ICryptoResult.cs
@@ -0,0 +1,10 @@
+﻿namespace NIST.CVP.Crypto.Common
+{
+    /// <summary>
+    /// Represents the return of a Crypto invoke
+    /// </summary>
+    public interface ICryptoResult
+    {
+        
+    }
+}
