4dbf33d71bc324ad00b1cc91df343bbf2614e5d4
Christopher Celi <christopher.celi@nist.gov>
2018-01-24 16:41:57 +0000

    Adds integration tests for ECDSA SigGen Component tests, but they need a special file from CAVS to run. Will not work with .rsp files from vendors.
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@391560 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- ecdsa/test/NIST.CVP.Generation.DSA.ECC.SigGen.IntegrationTests/FireHoseTests.cs
+++ ecdsa/test/NIST.CVP.Generation.DSA.ECC.SigGen.IntegrationTests/FireHoseTests.cs
@@ -14,12 +14,14 @@ namespace NIST.CVP.Generation.DSA.ECC.SigGen.IntegrationTests
     public class FireHoseTests
     {
         string _testPath;
+        private string _testPathComponent;
         IShaFactory _shaFactory;
 
         [SetUp]
         public void SetUp()
         {
             _testPath = Utilities.GetConsistentTestingStartPath(GetType(), @"..\..\TestFiles\LegacyParserFiles\siggen\");
+            _testPathComponent = Utilities.GetConsistentTestingStartPath(GetType(), @"..\..\TestFiles\LegacyParserFiles\siggencomponent\");
             _shaFactory = new ShaFactory();
         }
 
@@ -66,6 +68,59 @@ namespace NIST.CVP.Generation.DSA.ECC.SigGen.IntegrationTests
                         algo.AddEntropy(testCase.K);
 
                         var result = algo.Sign(testGroup.DomainParameters, testCase.KeyPair, testCase.Message);
+                        if (result.Signature.R != testCase.Signature.R || result.Signature.S != testCase.Signature.S)
+                        {
+                            Assert.Fail($"Could not validate TestCase: {testCase.TestCaseId}, with values: \nR: {testCase.Signature.R}\nS: {testCase.Signature.S}");
+                        }
+                    }
+                }
+            }
+        }
+
+        [Test]
+        [Ignore("Need to generate special file in CAVS for this")]
+        public void ShouldRunThroughTestFilesAndValidateSigGenComponent()
+        {
+            if (!Directory.Exists(_testPathComponent))
+            {
+                Assert.Fail("Test File Directory does not exist");
+            }
+
+            var folderPath = new DirectoryInfo(Path.Combine(_testPathComponent));
+            var parser = new LegacyResponseFileParser();
+
+            foreach (var testFilePath in folderPath.EnumerateFiles())
+            {
+                var parseResult = parser.Parse(testFilePath.FullName);
+                if (!parseResult.Success)
+                {
+                    Assert.Fail($"Could not parse: {testFilePath.FullName}");
+                }
+
+                var testVector = parseResult.ParsedObject;
+                if (testVector.TestGroups.Count == 0)
+                {
+                    Assert.Fail("No TestGroups parsed");
+                }
+
+                foreach (var iTestGroup in testVector.TestGroups)
+                {
+                    var testGroup = (TestGroup)iTestGroup;
+
+                    if (testGroup.Tests.Count == 0)
+                    {
+                        Assert.Fail("No TestCases parsed");
+                    }
+
+                    var sha = _shaFactory.GetShaInstance(testGroup.HashAlg);
+                    var algo = new EccDsa(sha, EntropyProviderTypes.Testable);
+
+                    foreach (var iTestCase in testGroup.Tests)
+                    {
+                        var testCase = (TestCase)iTestCase;
+                        algo.AddEntropy(testCase.K);
+
+                        var result = algo.Sign(testGroup.DomainParameters, testCase.KeyPair, testCase.Message, true);
                         if (result.Signature.R != testCase.Signature.R || result.Signature.S != testCase.Signature.S)
                         {
                             Assert.Fail($"Could not validate TestCase: {testCase.TestCaseId}, with values: \nR: {testCase.Signature.R}\nS: {testCase.Signature.S}");
