43c14556c2915fa6af233aecbd5f01a12c1cc04f
Russell Hammett <russell.hammett@nist.gov>
2018-02-04 19:06:59 +0000

    TDES-CBC to single app runner
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@391718 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ tdes-cbc/src/NIST.CVP.Generation.TDES_CBC/RegisterInjections.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using NIST.CVP.Common;
+using NIST.CVP.Crypto.TDES_CBC;
+using NIST.CVP.Generation.Core;
+using NIST.CVP.Generation.Core.Parsers;
+using NIST.CVP.Math;
+
+namespace NIST.CVP.Generation.TDES_CBC
+{
+    public class RegisterInjections : IRegisterInjections
+    {
+        public void RegisterTypes(ContainerBuilder builder)
+        {
+            builder.RegisterType<Generator<Parameters, TestVectorSet, TestGroup, TestCase>>().AsImplementedInterfaces();
+
+            builder.RegisterType<TdesCbc>().AsImplementedInterfaces();
+            builder.RegisterType<TDES_CBC_MCT>().AsImplementedInterfaces();
+            builder.RegisterType<MonteCarloKeyMaker>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorKnownAnswer>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactoryFactory<TestVectorSet, TestGroup, TestCase>>().AsImplementedInterfaces();
+
+            builder.RegisterType<TestVectorFactory<Parameters, TestVectorSet>>().AsImplementedInterfaces();
+            builder.RegisterType<TestGroupGeneratorFactory>().AsImplementedInterfaces();
+
+            builder.RegisterType<ParameterValidator>().AsImplementedInterfaces();
+            builder.RegisterType<ParameterParser<Parameters>>().AsImplementedInterfaces();
+            builder.RegisterType<Random800_90>().AsImplementedInterfaces();
+
+            builder.RegisterType<Validator<TestVectorSet, TestCase>>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseValidatorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<DynamicParser>().AsImplementedInterfaces();
+            builder.RegisterType<ResultValidator<TestCase>>().AsImplementedInterfaces();
+            builder.RegisterType<TestReconstitutor>().AsImplementedInterfaces();
+        }
+    }
+}
--- tdes-cbc/test/NIST.CVP.Generation.TDES_CBC.IntegrationTests/GenValTests.cs
+++ tdes-cbc/test/NIST.CVP.Generation.TDES_CBC.IntegrationTests/GenValTests.cs
@@ -3,27 +3,27 @@ using Autofac;
 using NIST.CVP.Generation.Core;
 using NIST.CVP.Generation.Core.Tests;
 using NIST.CVP.Generation.Core.Tests.Fakes;
+using NIST.CVP.Generation.GenValApp.Helpers;
 using NIST.CVP.Math;
 using NIST.CVP.Tests.Core.TestCategoryAttributes;
 using NUnit.Framework;
-using tdes_cbc;
 
 namespace NIST.CVP.Generation.TDES_CBC.IntegrationTests
 {
+
     [TestFixture, LongRunningIntegrationTest]
-    public class GenValTests : GenValTestsBase
+    public class GenValTests : GenValTestsSingleRunnerBase
     {
         public override string Algorithm { get; } = "TDES";
         public override string Mode { get; } = "CBC";
 
-        public override Executable Generator => Program.Main;
-        public override Executable Validator => TDES_CBC_Val.Program.Main;
+        public override Executable Generator => GenValApp.Program.Main;
+        public override Executable Validator => GenValApp.Program.Main;
 
         [SetUp]
         public override void SetUp()
         {
             AutofacConfig.OverrideRegistrations = null;
-            TDES_CBC_Val.AutofacConfig.OverrideRegistrations = null;
         }
 
         protected override void OverrideRegistrationGenFakeFailure()
@@ -36,7 +36,7 @@ namespace NIST.CVP.Generation.TDES_CBC.IntegrationTests
 
         protected override void OverrideRegistrationValFakeFailure()
         {
-            TDES_CBC_Val.AutofacConfig.OverrideRegistrations = builder =>
+            AutofacConfig.OverrideRegistrations = builder =>
             {
                 builder.RegisterType<FakeFailureDynamicParser>().AsImplementedInterfaces();
             };
@@ -44,7 +44,7 @@ namespace NIST.CVP.Generation.TDES_CBC.IntegrationTests
 
         protected override void OverrideRegistrationValFakeException()
         {
-            TDES_CBC_Val.AutofacConfig.OverrideRegistrations = builder =>
+            AutofacConfig.OverrideRegistrations = builder =>
             {
                 builder.RegisterType<FakeExceptionDynamicParser>().AsImplementedInterfaces();
             };
