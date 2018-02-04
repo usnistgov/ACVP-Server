ee85a4836b6560b09acc3e3bd32314dece702e13
Russell Hammett <russell.hammett@nist.gov>
2018-02-04 19:30:58 +0000

    TDES-CBCI to single app runner
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@391719 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- tdes-cbci/test/NIST.CVP.Generation.TDES_CBCI.IntegrationTests/GenValTests.cs
+++ tdes-cbci/test/NIST.CVP.Generation.TDES_CBCI.IntegrationTests/GenValTests.cs
@@ -6,26 +6,25 @@ using NIST.CVP.Math;
 using NIST.CVP.Tests.Core.TestCategoryAttributes;
 using NUnit.Framework;
 using System.Collections.Generic;
-using TDES_CBCI;
+using NIST.CVP.Generation.GenValApp.Helpers;
 
 namespace NIST.CVP.Generation.TDES_CBCI.IntegrationTests
 {
 
     [TestFixture, LongRunningIntegrationTest]
-    public class GenValTests : GenValTestsBase
+    public class GenValTests : GenValTestsSingleRunnerBase
     {
 
         public override string Algorithm { get; } = "TDES";
         public override string Mode { get; } = "CBCI";
 
-        public override Executable Generator => Program.Main;
-        public override Executable Validator => TDES_CBCI_Val.Program.Main;
+        public override Executable Generator => GenValApp.Program.Main;
+        public override Executable Validator => GenValApp.Program.Main;
 
         [SetUp]
         public override void SetUp()
         {
             AutofacConfig.OverrideRegistrations = null;
-            TDES_CBCI_Val.AutofacConfig.OverrideRegistrations = null;
         }
 
         protected override void OverrideRegistrationGenFakeFailure()
@@ -38,7 +37,7 @@ namespace NIST.CVP.Generation.TDES_CBCI.IntegrationTests
 
         protected override void OverrideRegistrationValFakeFailure()
         {
-            TDES_CBCI_Val.AutofacConfig.OverrideRegistrations = builder =>
+            AutofacConfig.OverrideRegistrations = builder =>
             {
                 builder.RegisterType<FakeFailureDynamicParser>().AsImplementedInterfaces();
             };
@@ -46,7 +45,7 @@ namespace NIST.CVP.Generation.TDES_CBCI.IntegrationTests
 
         protected override void OverrideRegistrationValFakeException()
         {
-            TDES_CBCI_Val.AutofacConfig.OverrideRegistrations = builder =>
+            AutofacConfig.OverrideRegistrations = builder =>
             {
                 builder.RegisterType<FakeExceptionDynamicParser>().AsImplementedInterfaces();
             };
