5c266a2bd4143d8e34fb08ea2be075bb5a72a903
Russell Hammett <russell.hammett@nist.gov>
2018-02-05 17:14:15 +0000

    TDES-ECB to single app runner
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@391726 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ tdes-ecb/src/NIST.CVP.Generation.TDES_ECB/RegisterInjections.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using NIST.CVP.Common;
+using NIST.CVP.Crypto.TDES_ECB;
+using NIST.CVP.Generation.Core;
+using NIST.CVP.Generation.Core.Parsers;
+using NIST.CVP.Math;
+
+namespace NIST.CVP.Generation.TDES_ECB
+{
+    public class RegisterInjections : IRegisterInjections
+    {
+        public void RegisterTypes(ContainerBuilder builder)
+        {
+            builder.RegisterType<Generator<Parameters, TestVectorSet, TestGroup, TestCase>>().AsImplementedInterfaces();
+
+            builder.RegisterType<Crypto.TDES_ECB.TDES_ECB>().AsImplementedInterfaces();
+            builder.RegisterType<TDES_ECB_MCT>().AsImplementedInterfaces();
+            builder.RegisterType<MonteCarloKeyMaker>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorKnownAnswer>().AsImplementedInterfaces();
+            builder.RegisterType<TestVectorFactory<Parameters, TestVectorSet>>().AsImplementedInterfaces();
+            builder.RegisterType<TestGroupGeneratorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseGeneratorFactoryFactory<TestVectorSet, TestGroup, TestCase>>().AsImplementedInterfaces();
+
+            builder.RegisterType<ParameterValidator>().AsImplementedInterfaces();
+            builder.RegisterType<ParameterParser<Parameters>>().AsImplementedInterfaces();
+            builder.RegisterType<Random800_90>().AsImplementedInterfaces();
+
+            builder.RegisterType<Validator<TestVectorSet, TestCase>>().AsImplementedInterfaces();
+            builder.RegisterType<TestCaseValidatorFactory>().AsImplementedInterfaces();
+            builder.RegisterType<DynamicParser>().AsImplementedInterfaces();
+            builder.RegisterType<ResultValidator<TestCase>>().AsImplementedInterfaces();
+            builder.RegisterType<TestReconstitutor>().AsImplementedInterfaces();
+        }
+    }
+}
