9a6306c5b381ae8a76ca3bfe86d1d3a91bf3013d
Christopher Celi <christopher.celi@nist.gov>
2018-02-12 16:59:52 +0000

    Fixes failing genval test
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@391836 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- rsa/test/NIST.CVP.Generation.RSA-SPComponent.IntegrationTests/GenValTests.cs
+++ rsa/test/NIST.CVP.Generation.RSA-SPComponent.IntegrationTests/GenValTests.cs
@@ -63,10 +63,19 @@ namespace NIST.CVP.Generation.RSA_SPComponent.IntegrationTests
             }
 
             var rand = new Random800_90();
-
+            // If TC has a signature, change it
             if (testCase.signature != null)
             {
-                testCase.signature = rand.GetDifferentBitStringOfSameSize(new BitString((string)testCase.signature)).ToHex();
+                var bs = new BitString(testCase.signature.ToString());
+                bs = rand.GetDifferentBitStringOfSameSize(bs);
+
+                // Can't get something "different" of empty bitstring of the same length
+                if (bs == null)
+                {
+                    bs = new BitString("01");
+                }
+
+                testCase.signature = bs.ToHex();
             }
         }
 
