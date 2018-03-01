84d3dd735a375f5941541555d785d12395315721
Russell Hammett <russell.hammett@nist.gov>
2018-03-01 19:06:51 +0000

    AES-CFB128 - Removing "NIST.CVP.Crypto" as a dependency of Generation assemblies
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/branches/branchOfRefactor@392101 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- aes-cfb128/src/NIST.CVP.Generation.AES_CFB128/TestCaseGeneratorKnownAnswer.cs
+++ aes-cfb128/src/NIST.CVP.Generation.AES_CFB128/TestCaseGeneratorKnownAnswer.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using NIST.CVP.Common.ExtensionMethods;
-using NIST.CVP.Crypto.AES;
-using NIST.CVP.Crypto.AES_CFB128;
 using NIST.CVP.Crypto.Common.Symmetric;
+using NIST.CVP.Crypto.Common.Symmetric.AES.KATs;
 using NIST.CVP.Generation.Core;
-using NIST.CVP.Math;
 
 namespace NIST.CVP.Generation.AES_CFB128
 {
--- aes-cfb128/src/NIST.CVP.Generation.AES_CFB128/TestCaseGeneratorMCTDecrypt.cs
+++ aes-cfb128/src/NIST.CVP.Generation.AES_CFB128/TestCaseGeneratorMCTDecrypt.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using NIST.CVP.Crypto.AES;
-using NIST.CVP.Crypto.AES_CFB128;
 using NIST.CVP.Crypto.Common.Symmetric;
 using NIST.CVP.Crypto.Common.Symmetric.AES;
 using NIST.CVP.Generation.Core;
