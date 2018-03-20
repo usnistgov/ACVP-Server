83668419b42d84293fcd22305ebf07e954e1c723
Christopher Celi <christopher.celi@nist.gov>
2018-03-20 20:27:37 +0000

    Splits apart GenValAppRunner into several testable components and adds tests for them.
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/trunk@392390 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ common/test/NIST.CVP.Generation.GenValApp.Tests/Fakes/FakeAutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace NIST.CVP.Generation.GenValApp.Tests.Fakes
+{
+    public class FakeAutofacConfig
+    {
+        private IContainer _container;
+
+        public IContainer GetContainer()
+        {
+            IoCConfiguration();
+            return _container;
+        }
+
+        private void IoCConfiguration()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<FakeGenerator>().AsImplementedInterfaces();
+            builder.RegisterType<FakeValidator>().AsImplementedInterfaces();
+
+            _container = builder.Build();
+        }
+    }
+}
---
+++ common/test/NIST.CVP.Generation.GenValApp.Tests/Fakes/FakeGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NIST.CVP.Generation.Core;
+
+namespace NIST.CVP.Generation.GenValApp.Tests.Fakes
+{
+    public class FakeGenerator : IGenerator
+    {
+        public GenerateResponse Generate(string requestFilePath)
+        {
+            if (requestFilePath.Contains("bad"))
+            {
+                return new GenerateResponse("fail");
+            }
+
+            return new GenerateResponse();
+        }
+    }
+}
