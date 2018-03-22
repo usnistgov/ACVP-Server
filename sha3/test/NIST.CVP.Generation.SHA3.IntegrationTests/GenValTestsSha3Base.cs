e600c9003454c79aa6c432d46e3cad46bf5e2cac
Christopher Celi <christopher.celi@nist.gov>
2018-03-22 16:10:11 +0000

    Fixes merge conflict in SHA3
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/branches/branchOfRefactor@392438 58cf25f4-cab2-a041-9f61-d3785993b9bc

---
+++ sha3/test/NIST.CVP.Generation.SHA3.IntegrationTests/GenValTestsSha3Base.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Autofac;
+using NIST.CVP.Common;
+using NIST.CVP.Generation.Core;
+using NIST.CVP.Generation.Core.Tests;
+using NIST.CVP.Math;
+using NIST.CVP.Math.Domain;
+using NIST.CVP.Tests.Core.TestCategoryAttributes;
+using NUnit.Framework;
+
+namespace NIST.CVP.Generation.SHA3.IntegrationTests
+{
+    [TestFixture, LongRunningIntegrationTest]
+    public abstract class GenValTestsSha3Base : GenValTestsSingleRunnerBase
+    {
+        public override IRegisterInjections RegistrationsGenVal => new RegisterInjections();
+        public override IRegisterInjections RegistrationsCrypto => new Crypto.RegisterInjections();
+
+        protected override void ModifyTestCaseToFail(dynamic testCase)
+        {
+            var rand = new Random800_90();
+
+            if (testCase.md != null)
+            {
+                var bs = new BitString(testCase.md.ToString());
+                bs = rand.GetDifferentBitStringOfSameSize(bs);
+                testCase.md = bs.ToHex();
+            }
+
+            if (testCase.msg != null)
+            {
+                var bs = new BitString(testCase.msg.ToString());
+                bs = rand.GetDifferentBitStringOfSameSize(bs);
+                testCase.msg = bs.ToHex();
+            }
+
+            if (testCase.resultsArray != null)
+            {
+                var bsMessage = new BitString(testCase.resultsArray[0].msg.ToString());
+                bsMessage = rand.GetDifferentBitStringOfSameSize(bsMessage);
+                testCase.resultsArray[0].msg = bsMessage.ToHex();
+
+                var bsDigest = new BitString(testCase.resultsArray[0].md.ToString());
+                bsDigest = rand.GetDifferentBitStringOfSameSize(bsDigest);
+                testCase.resultsArray[0].md = bsDigest.ToHex();
+            }
+        }
+
+        /// <summary>
+        /// Can be used to only generate AFT groups for the genval tests
+        /// </summary>
+        public class FakeTestGroupGeneratorFactory : ITestGroupGeneratorFactory<Parameters, TestGroup, TestCase>
+        {
+            public IEnumerable<ITestGroupGenerator<Parameters, TestGroup, TestCase>> GetTestGroupGenerators()
+            {
+                return new List<ITestGroupGenerator<Parameters, TestGroup, TestCase>>()
+                {
+                    new TestGroupGeneratorAlgorithmFunctional()
+                };
+            }
+        }
+        
+        protected override void OverrideRegisteredDependencies(ContainerBuilder builder)
+        {
+            builder.RegisterType<FakeTestGroupGeneratorFactory>().AsImplementedInterfaces();
+        }
+    }
+}
