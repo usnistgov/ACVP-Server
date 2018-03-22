7d557f6248e56a54a8c6f1e2ca19adb2efdaf66f
Russell Hammett <russell.hammett@nist.gov>
2018-03-22 19:23:41 +0000

    Merged revision(s) 392415-392424 from STVM_Research/ACVP/gen-val/trunk: Updates a lot of json files with the new naming convention ........ Adds TestType to AES_XTS ........ Removes unneeded files. ........
    git-svn-id: https://subversion.nist.gov/JIRA_repo1/STVM_Research/ACVP/gen-val/branches/branchOfRefactor@392448 58cf25f4-cab2-a041-9f61-d3785993b9bc

--- aes-xts/src/NIST.CVP.Generation.AES_XTS/TestGroupGenerator.cs
+++ aes-xts/src/NIST.CVP.Generation.AES_XTS/TestGroupGenerator.cs
@@ -10,6 +10,8 @@ namespace NIST.CVP.Generation.AES_XTS
 {
     public class TestGroupGenerator : ITestGroupGenerator<Parameters, TestGroup, TestCase>
     {
+        private const string TEST_TYPE_LABEL = "aft";
+
         public IEnumerable<TestGroup> BuildTestGroups(Parameters parameters)
         {
             var testGroups = new List<TestGroup>();
@@ -36,6 +38,7 @@ namespace NIST.CVP.Generation.AES_XTS
                                 KeyLen = keyLen,
                                 TweakMode = tweakMode,
                                 PtLen = ptLen,
+                                TestType = TEST_TYPE_LABEL
                             };
 
                             testGroups.Add(testGroup);
