51af6621721495bcdd41d0e2a500aa98f3537929
ctc <christopher.celi@nist.gov>
2018-05-14 14:01:51 -0400

    Adds expected and provided to AES_CCM and adds default value to showExpected (false)

--- src/aes-ccm/src/NIST.CVP.Generation.AES_CCM/TestCaseValidatorDecrypt.cs
+++ src/aes-ccm/src/NIST.CVP.Generation.AES_CCM/TestCaseValidatorDecrypt.cs
@@ -14,15 +14,21 @@ namespace NIST.CVP.Generation.AES_CCM
 
         public int TestCaseId => _expectedResult.TestCaseId;
 
-        public TestCaseValidation Validate(TestCase suppliedResult)
+        public TestCaseValidation Validate(TestCase suppliedResult, bool showExpected)
         {
             var errors = new List<string>();
+            var expected = new Dictionary<string, string>();
+            var provided = new Dictionary<string, string>();
+
             if (_expectedResult.TestPassed != null && !_expectedResult.TestPassed.Value)
             {
                 if (!suppliedResult.TestPassed.HasValue || 
                     (suppliedResult.TestPassed != null && suppliedResult.TestPassed.Value))
                 {
                     errors.Add("Expected tag validation failure");
+                    expected.Add(nameof(_expectedResult.TestPassed), _expectedResult.TestPassed.Value.ToString());
+                    provided.Add(nameof(suppliedResult.TestPassed),
+                        suppliedResult.TestPassed.HasValue ? suppliedResult.TestPassed.ToString() : "none");
                 }
             }
             else
@@ -30,13 +36,20 @@ namespace NIST.CVP.Generation.AES_CCM
                 ValidateResultPresent(suppliedResult, errors);
                 if (errors.Count == 0)
                 {
-                    CheckResults(suppliedResult, errors);
+                    CheckResults(suppliedResult, errors, expected, provided);
                 }
             }
 
             if (errors.Count > 0)
             {
-                return new TestCaseValidation { TestCaseId = suppliedResult.TestCaseId, Result = Core.Enums.Disposition.Failed, Reason = string.Join("; ", errors) };
+                return new TestCaseValidation
+                {
+                    TestCaseId = suppliedResult.TestCaseId, 
+                    Result = Core.Enums.Disposition.Failed, 
+                    Reason = string.Join("; ", errors),
+                    Expected = showExpected ? expected : null,
+                    Provided = showExpected ? provided : null
+                };
             }
             return  new TestCaseValidation { TestCaseId = suppliedResult.TestCaseId, Result = Core.Enums.Disposition.Passed};
         }
@@ -50,11 +63,13 @@ namespace NIST.CVP.Generation.AES_CCM
             }
         }
 
-        private void CheckResults(TestCase suppliedResult, List<string> errors)
+        private void CheckResults(TestCase suppliedResult, List<string> errors, Dictionary<string, string> expected, Dictionary<string, string> provided)
         {
             if (!_expectedResult.PlainText.Equals(suppliedResult.PlainText))
             {
                 errors.Add("Plain Text does not match");
+                expected.Add(nameof(_expectedResult.PlainText), _expectedResult.PlainText.ToHex());
+                provided.Add(nameof(suppliedResult.PlainText), suppliedResult.PlainText.ToHex());
             }
         }
     }
