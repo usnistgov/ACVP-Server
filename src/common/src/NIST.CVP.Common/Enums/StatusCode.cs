cfa8e845577a52812f47b8b7445cc38f9e3e841c
ctc <christopher.celi@nist.gov>
2018-05-18 15:35:56 -0400

    Updates messages for status codes

--- src/common/src/NIST.CVP.Common/Enums/StatusCode.cs
+++ src/common/src/NIST.CVP.Common/Enums/StatusCode.cs
@@ -25,16 +25,22 @@ namespace NIST.CVP.Common.Enums
         [EnumMember(Value = "invalid mode provided, must run generator or validator and provide needed files")]
         ModeError,
 
+        [EnumMember(Value = "parameter error, unable to parse parameters in registration file")]
         ParameterError,
-
+        
+        [EnumMember(Value = "parameter validator error, parameters do not match the required parameters for the algorithm, check appropriate values")]
         ParameterValidationError,
 
+        [EnumMember(Value = "file save error, unable to save json files")]
         FileSaveError,
 
+        [EnumMember(Value = "test case generator error, unable to produce test case for prompt file")]
         TestCaseGeneratorError,
 
+        [EnumMember(Value = "test case validator error, unable to parse provided results file, or expected value is missing")]
         TestCaseValidatorError,
 
+        [EnumMember(Value = "file read error, unable to read internal projection or results file")]
         FileReadError
     }
 }
