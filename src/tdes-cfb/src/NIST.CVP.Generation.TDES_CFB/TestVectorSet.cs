0df681fc4c377f1491155d81a0dab3b71ce4c9b8
ctc <christopher.celi@nist.gov>
2018-05-29 12:36:36 -0400

    Fixes missing line breaks and test group list

--- src/tdes-cfb/src/NIST.CVP.Generation.TDES_CFB/TestVectorSet.cs
+++ src/tdes-cfb/src/NIST.CVP.Generation.TDES_CFB/TestVectorSet.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections.Generic;
 using NIST.CVP.Generation.Core;
 
-
 namespace NIST.CVP.Generation.TDES_CFB
 {
-
     public class TestVectorSet : ITestVectorSet<TestGroup, TestCase>
     {
         public string Algorithm { get; set; } = "TDES";
         public string Mode { get; set; } = "CFB";
-        public bool IsSample { get; set; }    }
+        public bool IsSample { get; set; }
+        public List<TestGroup> TestGroups { get; set; } = new List<TestGroup>();
+    }
 }
