2c678441e1a4972763b536d99d97db932f9505cb
ctc <christopher.celi@nist.gov>
2018-06-18 12:25:09 -0400

    Updates AES ECB to use new crypto interfaces

--- src/aes-ecb/src/NIST.CVP.Generation.AES_ECB/TestCaseGeneratorFactory.cs
+++ src/aes-ecb/src/NIST.CVP.Generation.AES_ECB/TestCaseGeneratorFactory.cs
@@ -1,4 +1,8 @@
-﻿using NIST.CVP.Crypto.Common.Symmetric.AES;
+﻿using NIST.CVP.Crypto.Common.Symmetric;
+using NIST.CVP.Crypto.Common.Symmetric.BlockModes;
+using NIST.CVP.Crypto.Common.Symmetric.Engines;
+using NIST.CVP.Crypto.Common.Symmetric.Enums;
+using NIST.CVP.Crypto.Common.Symmetric.MonteCarlo;
 using NIST.CVP.Generation.Core;
 using NIST.CVP.Math;
 
@@ -7,14 +11,14 @@ namespace NIST.CVP.Generation.AES_ECB
     public class TestCaseGeneratorFactory : ITestCaseGeneratorFactory<TestGroup, TestCase>
     {
         private readonly IRandom800_90 _random800_90;
-        private readonly IAES_ECB _algo;
-        private readonly IAES_ECB_MCT _mctAlgo;
+        private readonly IModeBlockCipher<SymmetricCipherResult> _algo;
+        private readonly IMonteCarloTester<MCTResult<AlgoArrayResponse>, AlgoArrayResponse> _mctAlgo;
 
-        public TestCaseGeneratorFactory(IRandom800_90 random800_90, IAES_ECB algo, IAES_ECB_MCT mctAlgo)
+        public TestCaseGeneratorFactory(IRandom800_90 random800_90, IBlockCipherEngineFactory engineFactory, IModeBlockCipherFactory cipherFactory, IMonteCarloFactoryAes mctFactory)
         {
-            _algo = algo;
             _random800_90 = random800_90;
-            _mctAlgo = mctAlgo;
+            _algo = cipherFactory.GetStandardCipher(engineFactory.GetSymmetricCipherPrimitive(BlockCipherEngines.Aes), BlockCipherModesOfOperation.Ecb);
+            _mctAlgo = mctFactory.GetInstance(BlockCipherModesOfOperation.Ecb);
         }
 
         public ITestCaseGenerator<TestGroup, TestCase> GetCaseGenerator(TestGroup testGroup)
--- src/aes-ecb/src/NIST.CVP.Generation.AES_ECB/TestCaseGeneratorMMTDecrypt.cs
+++ src/aes-ecb/src/NIST.CVP.Generation.AES_ECB/TestCaseGeneratorMMTDecrypt.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NIST.CVP.Crypto.Common.Symmetric;
-using NIST.CVP.Crypto.Common.Symmetric.AES;
+using NIST.CVP.Crypto.Common.Symmetric.BlockModes;
+using NIST.CVP.Crypto.Common.Symmetric.Enums;
 using NIST.CVP.Generation.Core;
 using NIST.CVP.Math;
 using NLog;
@@ -9,7 +10,7 @@ namespace NIST.CVP.Generation.AES_ECB
 {
     public class TestCaseGeneratorMMTDecrypt : ITestCaseGenerator<TestGroup, TestCase>
     {
-        private readonly IAES_ECB _algo;
+        private readonly IModeBlockCipher<SymmetricCipherResult> _algo;
         private readonly IRandom800_90 _random800_90;
 
         private const int _PT_LENGTH_MULTIPLIER = 16;
@@ -19,7 +20,7 @@ namespace NIST.CVP.Generation.AES_ECB
 
         public int NumberOfTestCasesToGenerate => 10;
 
-        public TestCaseGeneratorMMTDecrypt(IRandom800_90 random800_90, IAES_ECB algo)
+        public TestCaseGeneratorMMTDecrypt(IRandom800_90 random800_90, IModeBlockCipher<SymmetricCipherResult> algo)
         {
             _random800_90 = random800_90;
             _algo = algo;
@@ -41,15 +42,16 @@ namespace NIST.CVP.Generation.AES_ECB
 
         public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup @group, TestCase testCase)
         {
-            SymmetricCipherResult encryptionResult = null;
+            SymmetricCipherResult decryptionResult = null;
             try
             {
-                encryptionResult = _algo.BlockEncrypt(testCase.Key, testCase.PlainText);
-                if (!encryptionResult.Success)
+                var param = new ModeBlockCipherParameters(BlockCipherDirections.Encrypt, testCase.Key, testCase.PlainText);
+                decryptionResult = _algo.ProcessPayload(param);
+                if (!decryptionResult.Success)
                 {
-                    ThisLogger.Warn(encryptionResult.ErrorMessage);
+                    ThisLogger.Warn(decryptionResult.ErrorMessage);
                     {
-                        return new TestCaseGenerateResponse<TestGroup, TestCase>(encryptionResult.ErrorMessage);
+                        return new TestCaseGenerateResponse<TestGroup, TestCase>(decryptionResult.ErrorMessage);
                     }
                 }
             }
@@ -60,13 +62,10 @@ namespace NIST.CVP.Generation.AES_ECB
                     return new TestCaseGenerateResponse<TestGroup, TestCase>(ex.Message);
                 }
             }
-            testCase.CipherText = encryptionResult.Result;
-           
-         
+            testCase.CipherText = decryptionResult.Result;
 
             return new TestCaseGenerateResponse<TestGroup, TestCase>(testCase);
         }
-
 
         private Logger ThisLogger => LogManager.GetCurrentClassLogger();
     }
