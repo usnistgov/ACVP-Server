a727d68029b8050162582c57c818d0790d779257
Matt Watson <mpw4@NIST.GOV>
2018-06-28 14:25:58 -0400

    Fixes part of KMAC tests

--- src/crypto/test/NIST.CVP.Crypto.KMAC.Tests/KmacTests.cs
+++ src/crypto/test/NIST.CVP.Crypto.KMAC.Tests/KmacTests.cs
@@ -28,14 +28,14 @@ namespace NIST.CVP.Crypto.KMAC.Tests
         [TestCase(256, 256, 2407, "404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c", "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f", "dea457f7eb7469f63f02b018da7400060d6164ef230bdeb7e73ccd8464f38725", "My Function")]
         public void ShouldKMAC128Correctly(int length, int macLength, int keyLen, string key, string inputHex, string outputHex, string customization)
         {
-            var message = new BitString(inputHex, length, false);
+            var message = new BitString(inputHex, length, true);
             var expectedResult = new BitString(outputHex, macLength, false);
-            var hashFunction = GetCSHAKEHashFunction(macLength, 256);
+            var hashFunction = GetCSHAKEHashFunction(macLength, 256, customization);
 
             var CSHAKEWrapped = _cSHAKEFactory.GetCSHAKE(hashFunction);
 
             var subject = new Kmac(CSHAKEWrapped, 256, false);
-            var result = subject.Generate(new BitString(key, keyLen, true), message, customization, 256);
+            var result = subject.Generate(new BitString(key, keyLen, true), message, customization, macLength);
 
             Assume.That(result.Success);
             Assert.AreEqual(expectedResult, result.Mac);
@@ -52,12 +52,12 @@ namespace NIST.CVP.Crypto.KMAC.Tests
         {
             var message = new BitString(inputHex, length, false);
             var expectedResult = new BitString(outputHex, macLength, false);
-            var hashFunction = GetCSHAKEHashFunction(macLength, 512);
+            var hashFunction = GetCSHAKEHashFunction(macLength, 512, customization);
 
             var CSHAKEWrapped = _cSHAKEFactory.GetCSHAKE(hashFunction);
 
             var subject = new Kmac(CSHAKEWrapped, 512, false);
-            var result = subject.Generate(new BitString(key, keyLen, true), message, customization, 512);
+            var result = subject.Generate(new BitString(key, keyLen, true), message, customization, macLength);
 
             Assume.That(result.Success);
             Assert.AreEqual(expectedResult, result.Mac);
@@ -77,7 +77,7 @@ namespace NIST.CVP.Crypto.KMAC.Tests
         {
             var message = new BitString(inputHex, length, false);
             var expectedResult = new BitString(outputHex, macLength, false);
-            var hashFunction = GetCSHAKEHashFunction(macLength, 256);
+            var hashFunction = GetCSHAKEHashFunction(macLength, 256, customization);
 
             var CSHAKEWrapped = _cSHAKEFactory.GetCSHAKE(hashFunction);
 
@@ -102,7 +102,7 @@ namespace NIST.CVP.Crypto.KMAC.Tests
         {
             var message = new BitString(inputHex, length, false);
             var expectedResult = new BitString(outputHex, macLength, false);
-            var hashFunction = GetCSHAKEHashFunction(macLength, 512);
+            var hashFunction = GetCSHAKEHashFunction(macLength, 512, customization);
 
             var CSHAKEWrapped = _cSHAKEFactory.GetCSHAKE(hashFunction);
 
@@ -113,13 +113,14 @@ namespace NIST.CVP.Crypto.KMAC.Tests
             Assert.AreEqual(expectedResult, result.Mac);
         }
 
-        private HashFunction GetCSHAKEHashFunction(int digestSize, int capacity)
+        private HashFunction GetCSHAKEHashFunction(int digestSize, int capacity, string customization)
         {
             return new HashFunction()
             {
                 DigestSize = digestSize,
                 Capacity = capacity,
-                XOF = true
+                Customization = customization,
+                FunctionName = "KMAC"
             };
         }
     }
