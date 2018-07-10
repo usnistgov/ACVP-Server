44a0e1717dcdde26fbf54e617086a8859cc33c97
ctc <christopher.celi@nist.gov>
2018-07-10 17:26:18 -0400

    Adds CTR methods to Oracle and updates gen/vals for AES CTR

---
+++ src/aes-ctr/src/NIST.CVP.Generation.AES_CTR/DeferredIvExtractor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NIST.CVP.Common.Oracle;
+using NIST.CVP.Common.Oracle.ParameterTypes;
+using NIST.CVP.Common.Oracle.ResultTypes;
+using NIST.CVP.Crypto.Common.Symmetric;
+using NIST.CVP.Generation.Core;
+
+namespace NIST.CVP.Generation.AES_CTR
+{
+    public class DeferredIvExtractor : IDeferredTestCaseResolver<TestGroup, TestCase, SymmetricCounterResult>
+    {
+        private readonly IOracle _oracle;
+
+        public DeferredIvExtractor(IOracle oracle)
+        {
+            _oracle = oracle;
+        }
+
+        public SymmetricCounterResult CompleteDeferredCrypto(TestGroup serverTestGroup, TestCase serverTestCase, TestCase iutTestCase)
+        {
+            var param = new AesParameters
+            {
+                Direction = serverTestGroup.Direction,
+                KeyLength = serverTestGroup.KeyLength
+            };
+
+            var fullParam = new AesResult
+            {
+                Key = serverTestCase.Key,
+                Iv = serverTestCase.IV,
+                PlainText = serverTestGroup.Direction.ToLower() == "encrypt"
+                    ? serverTestCase.PlainText
+                    : iutTestCase.PlainText,
+                CipherText = serverTestGroup.Direction.ToLower() == "decrypt"
+                    ? serverTestCase.CipherText
+                    : iutTestCase.CipherText
+            };
+
+            try
+            {
+                var result = _oracle.ExtractIvs(param, fullParam);
+
+                return new SymmetricCounterResult(
+                    serverTestGroup.Direction.ToLower() == "encrypt" ? result.CipherText : result.PlainText,
+                    result.IVs);
+            }
+            catch (Exception ex)
+            {
+                return new SymmetricCounterResult($"Unable to compute IVs. {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/aes-ctr/src/NIST.CVP.Generation.AES_CTR/TestCaseGeneratorPartialBlock.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NIST.CVP.Common.Oracle;
+using NIST.CVP.Common.Oracle.ParameterTypes;
+using NIST.CVP.Common.Oracle.ResultTypes;
+using NIST.CVP.Crypto.Common.Symmetric.Enums;
+using NIST.CVP.Generation.Core;
+using NIST.CVP.Math.Domain;
+using NLog;
+
+namespace NIST.CVP.Generation.AES_CTR
+{
+    public class TestCaseGeneratorPartialBlock : ITestCaseGenerator<TestGroup, TestCase>
+    {
+        private readonly IOracle _oracle;
+
+        private int _casesPerSize = 5;
+        private bool _sizesSet = false;
+        private List<int> _validSizes = new List<int>();
+        private int _curCasePerSizeIndex;
+        private int _curSizeIndex;
+
+        public int NumberOfTestCasesToGenerate { get; private set; } = 1;
+
+        public TestCaseGeneratorPartialBlock(IOracle oracle)
+        {
+            _oracle = oracle;
+        }
+
+        public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup group, bool isSample)
+        {
+            if (isSample)
+            {
+                _casesPerSize = 1;
+            }
+
+            // Only do this once as a way to make sure nothing changes
+            if (!_sizesSet)
+            {
+                _validSizes = GetValidSizes(group.DataLength);
+
+                // Must be set here because it depends on group information
+                NumberOfTestCasesToGenerate = _casesPerSize * _validSizes.Count;
+            }
+
+            if (_curCasePerSizeIndex >= _casesPerSize)
+            {
+                _curCasePerSizeIndex = 0;
+                _curSizeIndex++;
+            }
+
+            _curCasePerSizeIndex++;
+
+            var ctLen = _validSizes[_curSizeIndex];
+
+            // This is a little hacky... but single block CTR is the same as OFB. So we can get past the awkward factory
+            // TODO fix this up
+            var param = new AesParameters
+            {
+                DataLength = ctLen,
+                KeyLength = group.KeyLength,
+                Direction = group.Direction,
+                Mode = BlockCipherModesOfOperation.Ofb
+            };
+
+            AesResult result = null;
+            try
+            {
+                result = _oracle.GetAesCase(param);
+            }
+            catch (Exception ex)
+            {
+                return new TestCaseGenerateResponse<TestGroup, TestCase>($"Failed to generate. {ex.Message}");
+            }
+
+            return new TestCaseGenerateResponse<TestGroup, TestCase>(new TestCase
+            {
+                Key = result.Key,
+                IV = result.Iv,
+                PlainText = result.PlainText,
+                CipherText = result.CipherText
+            });
+        }
+
+        public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup group, TestCase testCase)
+        {
+            return null;
+        }
+
+        private Logger ThisLogger => LogManager.GetCurrentClassLogger();
+
+        private List<int> GetValidSizes(MathDomain dataLength)
+        {
+            _sizesSet = true;
+
+            // Can ask for 128 values because the valid domain only has this many elements
+            return dataLength.GetValues(ParameterValidator.MAXIMUM_DATA_LEN).ToList();
+        }
+    }
+}
