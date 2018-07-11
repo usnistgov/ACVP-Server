8e15b8e2919cdb6427bc34027c7bf12fd195ac55
ctc <christopher.celi@nist.gov>
2018-07-11 10:35:52 -0400

    Updates to AES-CFB 1, 8, 128

---
+++ src/aes-cfb128/src/NIST.CVP.Generation.AES_CFB128/TestCaseGeneratorMct.cs
@@ -0,0 +1,60 @@
+﻿using NIST.CVP.Common.Oracle;
+using NIST.CVP.Common.Oracle.ParameterTypes;
+using NIST.CVP.Common.Oracle.ResultTypes;
+using NIST.CVP.Crypto.Common.Symmetric;
+using NIST.CVP.Crypto.Common.Symmetric.Enums;
+using NIST.CVP.Generation.Core;
+using System;
+using System.Linq;
+
+namespace NIST.CVP.Generation.AES_CFB128
+{
+    public class TestCaseGeneratorMct : ITestCaseGenerator<TestGroup, TestCase>
+    {
+        private readonly IOracle _oracle;
+
+        public int NumberOfTestCasesToGenerate => 1;
+
+        public TestCaseGeneratorMct(IOracle oracle)
+        {
+            _oracle = oracle;
+        }
+
+        public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup group, bool isSample)
+        {
+            var param = new AesParameters
+            {
+                Mode = BlockCipherModesOfOperation.CfbBlock,
+                DataLength = 128,
+                Direction = group.Function,
+                KeyLength = group.KeyLength
+            };
+
+            MctResult<AesResult> oracleResult = null;
+            try
+            {
+                oracleResult = _oracle.GetAesMctCase(param);
+            }
+            catch (Exception ex)
+            {
+                return new TestCaseGenerateResponse<TestGroup, TestCase>($"Failed to generate. {ex.Message}");
+            }
+
+            return new TestCaseGenerateResponse<TestGroup, TestCase>(new TestCase
+            {
+                ResultsArray = Array.ConvertAll(oracleResult.Results.ToArray(), element => new AlgoArrayResponse
+                {
+                    IV = element.Iv,
+                    PlainText = element.PlainText,
+                    CipherText = element.CipherText,
+                    Key = element.Key
+                }).ToList()
+            });
+        }
+
+        public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup group, TestCase testCase)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/aes-cfb128/src/NIST.CVP.Generation.AES_CFB128/TestCaseGeneratorMmt.cs
@@ -0,0 +1,60 @@
+﻿using NIST.CVP.Common.Oracle;
+using NIST.CVP.Common.Oracle.ParameterTypes;
+using NIST.CVP.Common.Oracle.ResultTypes;
+using NIST.CVP.Crypto.Common.Symmetric.Enums;
+using NIST.CVP.Generation.Core;
+using System;
+
+namespace NIST.CVP.Generation.AES_CFB128
+{
+    public class TestCaseGeneratorMmt : ITestCaseGenerator<TestGroup, TestCase>
+    {
+        private readonly IOracle _oracle;
+
+        private const int LENGTH_MULTIPLIER = 16;
+        private const int BITS_IN_BYTE = 8;
+
+        private int _lenGenIteration = 1;
+
+        public int NumberOfTestCasesToGenerate => 10;
+
+        public TestCaseGeneratorMmt(IOracle oracle)
+        {
+            _oracle = oracle;
+        }
+
+        public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup group, bool isSample)
+        {
+            var param = new AesParameters
+            {
+                Mode = BlockCipherModesOfOperation.CfbBlock,
+                DataLength = _lenGenIteration++ * LENGTH_MULTIPLIER * BITS_IN_BYTE,
+                Direction = group.Function,
+                KeyLength = group.KeyLength
+            };
+
+            AesResult oracleResult = null;
+            try
+            {
+                oracleResult = _oracle.GetAesCase(param);
+            }
+            catch (Exception ex)
+            {
+                return new TestCaseGenerateResponse<TestGroup, TestCase>($"Failed to generate. {ex.Message}");
+            }
+
+            return new TestCaseGenerateResponse<TestGroup, TestCase>(new TestCase
+            {
+                Key = oracleResult.Key,
+                IV = oracleResult.Iv,
+                PlainText = oracleResult.PlainText,
+                CipherText = oracleResult.CipherText
+            });
+        }
+
+        public TestCaseGenerateResponse<TestGroup, TestCase> Generate(TestGroup @group, TestCase testCase)
+        {
+            return null;
+        }
+    }
+}
