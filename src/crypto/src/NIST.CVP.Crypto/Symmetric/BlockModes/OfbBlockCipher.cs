e376c56bd9e15ce112df05abb62e90c1ccc609f5
ctc <christopher.celi@nist.gov>
2018-07-11 12:02:25 -0400

    Updates OFB to properly encrypt/decrypt partial blocks

--- src/crypto/src/NIST.CVP.Crypto/Symmetric/BlockModes/OfbBlockCipher.cs
+++ src/crypto/src/NIST.CVP.Crypto/Symmetric/BlockModes/OfbBlockCipher.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using NIST.CVP.Crypto.Common.Symmetric;
+﻿using NIST.CVP.Crypto.Common.Symmetric;
 using NIST.CVP.Crypto.Common.Symmetric.BlockModes;
 using NIST.CVP.Crypto.Common.Symmetric.Engines;
 using NIST.CVP.Crypto.Common.Symmetric.Enums;
 using NIST.CVP.Math;
+using System;
 
 namespace NIST.CVP.Crypto.Symmetric.BlockModes
 {
@@ -34,34 +34,20 @@ namespace NIST.CVP.Crypto.Symmetric.BlockModes
 
         private void ProcessPayload(IModeBlockCipherParameters param, int numberOfBlocks, byte[] outBuffer)
         {
-            var payLoad = param.Payload.ToBytes();
+            // Concatenate payload to block boundary
+            var bitsInLastBlock = param.Payload.BitLength % _engine.BlockSizeBits;
+            var payLoad = param.Payload.GetDeepCopy().ConcatenateBits(BitString.Zeroes(_engine.BlockSizeBits - bitsInLastBlock)).ToBytes();
+
             var iv = param.Iv.GetDeepCopy().ToBytes();
             var ivOutBuffer = new byte[iv.Length];
-            var bytesInLastBlock = param.Payload.BitLength % _engine.BlockSizeBits / 8;
-            var xorBound = _engine.BlockSizeBytes;
 
             // For each block
             for (int i = 0; i < numberOfBlocks; i++)
             {
                 _engine.ProcessSingleBlock(iv, ivOutBuffer, 0);
 
-                // On the last block, only xor the bytes that are actually present
-                if (i == numberOfBlocks - 1)
-                {
-                    xorBound = bytesInLastBlock;
-                    if (param.Payload.BitLength == _engine.BlockSizeBits)
-                    {
-                        xorBound = _engine.BlockSizeBytes;
-                    }
-
-                    if (xorBound == 0)
-                    {
-                        xorBound = 1;
-                    }
-                }
-
                 // XOR processed IV onto current block payload
-                for (int j = 0; j < xorBound; j++)
+                for (int j = 0; j < _engine.BlockSizeBytes; j++)
                 {
                     outBuffer[i * _engine.BlockSizeBytes + j] =
                         (byte) (payLoad[i * _engine.BlockSizeBytes + j] ^ ivOutBuffer[j]);
