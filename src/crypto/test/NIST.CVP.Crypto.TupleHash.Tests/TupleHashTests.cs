d73bbeefa268ab8c8447c38397dc5db0d75728b3
Matt Watson <mpw4@NIST.GOV>
2018-07-26 14:35:28 -0400

    Fixes TupleHash test data

--- src/crypto/test/NIST.CVP.Crypto.TupleHash.Tests/TupleHashTests.cs
+++ src/crypto/test/NIST.CVP.Crypto.TupleHash.Tests/TupleHashTests.cs
@@ -12,11 +12,13 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
         [Test]
         [TestCase(1, 256, "c5d8786c1afb9b82111ab34b65b2c0048fa64e6d48e263264ce1707d3ffc8ed1", "")]
         [TestCase(1, 256, "75cdb20ff4db1154e841d758e24160c54bae86eb8c13e7f5f40eb35588e96dfb", "My Tuple App")]
+        [TestCase(1, 256, "b5ec70d703cc815b643bf50608b63cb8a424f739f693a2be8d6b72f0fa42df38", "My Function")]
         [TestCase(2, 256, "e60f202c89a2631eda8d4c588ca5fd07f39e5151998deccf973adb3804bb6e84", "My Tuple App")]
         [TestCase(1, 1607, "468e78a1302265b2de571d7c99f3aa4a1dbf07a901383af9f202070ddf10564e3e3f3556a960a4ba3e70a068433ebba80a646c334796fa6f812ea91a1cb58c83c1205d4b24d361b1b4b5bab0e346e0772535eaab06ca8a6a02fdceb45dfeec8d50ab78f70008d49e822ea5d45ed50292d6106243f2830f38a250578a3d95ea88068d0c32d6e9c14657bb25e36c52b24dcdef2bf574adc9a847d9bbe61882fc24eae573ac53cdc47198978b1018bce755ccc95c50d1cda01058bbcaac3ba170d2c88456bf5222da3753", "My Function")]
         [TestCase(1, 35, "1d68223b04", "My Function")]
-        //[TestCase(3, 256, "d7f108c25eb0c1ed5b9cdd004ead5afb72d807ed16c982a731b23a19c59f25e0", "My Function")]
+        [TestCase(3, 256, "c0cb5084d954a407b2eb6f3c826b343909a04b6112e1307ecba225be254e7182", "My Function")]
         [TestCase(4, 256, "9757bca16d3dfd035e1a586f1e8a34b8f6b26f241695527f6395385a0305030c", "My Function")]
+        [TestCase(5, 256, "443799edc7adba7ff5df756f7e392ac93a552055e452f336b7a6e76a255232ba", "My Function")]
         public void ShouldTupleHash128HashCorrectly(int testTupleId, int outputLength, string outputHex, string customization)
         {
             var tuple = GetTestTuple(testTupleId);
@@ -26,7 +28,7 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
 
             var subject = new TupleHash();
             var result = subject.HashMessage(hashFunction, tuple);
-            System.Console.WriteLine(result.ErrorMessage);
+
             Assume.That(result.Success);
             Assert.AreEqual(expectedResult, result.Digest);
         }
@@ -44,7 +46,7 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
 
             var subject = new TupleHash();
             var result = subject.HashMessage(hashFunction, tuple, customization);
-            System.Console.WriteLine(result.ErrorMessage);
+
             Assume.That(result.Success);
             Assert.AreEqual(expectedResult, result.Digest);
         }
@@ -55,7 +57,7 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
         [TestCase(2, 512, "45000be63f9b6bfd89f54717670f69a9bc763591a4f05c50d68891a744bcc6e7d6d5b5e82c018da999ed35b0bb49c9678e526abd8e85c13ed254021db9e790ce", "My Tuple App")]
         [TestCase(1, 1607, "156408acc04508d50ed83fd67060a108061e93f85cb6b923cbccd76388d5e4e0441283fb31095c831ac4de13793286957385625a00f2e78b03e681f42f12f772935a5c4dc16e99b69ab9bfc65f7a6f30dc7e4d723ce6eda552b116d6fb33da4c976bd9616c0f97cafc338e2c5a50b7692c95f65038446f83670846a70c5ad51a242db3e53922024f85d39352cc67cdac101c7ed7793c9b37a33d1027c628a682736fc268b5aeb1c8548b38099abf3a1e08c845246fe358d902842bff77dc1ae142aa8443c544aac124", "My Function")]
         [TestCase(1, 35, "745800a905", "My Function")]
-        //[TestCase(3, 256, "b95455cc71c4701f6e508c7d5a049d5baf192987b5f4e07e6b7d9944ff388997", "My Function")]
+        [TestCase(3, 256, "429e9236c1e153a2c8feec211b0fbfbab13642a4f4e982369863a13b727c075c", "My Function")]
         [TestCase(4, 256, "d763974ed5328c7b1bd00fb02c5480a90a5945b95e98b90f14b5581aecd0a426", "My Function")]
         public void ShouldTupleHash256HashCorrectly(int testTupleId, int outputLength, string outputHex, string customization)
         {
@@ -78,16 +80,16 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
         [TestCase(1, 128, "2f103cd7c32320353495c68de1a81292", "")]
         [TestCase(1, 128, "3fc8ad69453128292859a18b6c67d7ad", "My Tuple App")]
         [TestCase(2, 128, "900fe16cad098d28e74d632ed852f99d", "My Tuple App")]
-        //[TestCase(1, 1607, "468e78a1302265b2de571d7c99f3aa4a1dbf07a901383af9f202070ddf10564e3e3f3556a960a4ba3e70a068433ebba80a646c334796fa6f812ea91a1cb58c83c1205d4b24d361b1b4b5bab0e346e0772535eaab06ca8a6a02fdceb45dfeec8d50ab78f70008d49e822ea5d45ed50292d6106243f2830f38a250578a3d95ea88068d0c32d6e9c14657bb25e36c52b24dcdef2bf574adc9a847d9bbe61882fc24eae573ac53cdc47198978b1018bce755ccc95c50d1cda01058bbcaac3ba170d2c88456bf5222da3753", "My Function")]
-        //[TestCase(1, 35, "1d68223b04", "My Function")]
-        //[TestCase(3, 256, "d7f108c25eb0c1ed5b9cdd004ead5afb72d807ed16c982a731b23a19c59f25e0", "My Function")]
-        //[TestCase(4, 256, "9757bca16d3dfd035e1a586f1e8a34b8f6b26f241695527f6395385a0305030c", "My Function")]
-        public void ShouldTupleHashXOF128HashCorrectly(int testTupleId, int digestSize, string outputHex, string customization)
+        [TestCase(1, 1607, "a513a8a944047fdcfc441b7981f85b34e7b921d64e5cd30e5a8493600049124ffe004e4fa07a839c91baeb80ecb6582b47b0d40e169bca2de6232068f1e1c7e9e9cd35b61abdc00e92ecfa700f9933d6b14f6e0beccbec6fceca3a5c1b96e5221ff28e108db490b4ff14f3cda0f03bfbec4f0dca1e26cbb7709f0d73d43ab3668107cff8039386927994a6064828830653e9a9a6026e60b4f3e4445b3ea07b3857f2428c3904852f6a0cc4a477d95b774169a8fbedb6c3be2d7bbb60d1da09f904f396600e67f0ed31", "My Function")]
+        [TestCase(1, 35, "a513a8a904", "My Function")]
+        [TestCase(3, 256, "8524b653cfcdf36fa4c2885c91aaca20c152454dd9d98ed2299897cc7fa6561c", "My Function")]
+        [TestCase(4, 256, "ddc0561b588045ca71b728231b811d4d37287ec56cd6f57cd7316bf25fe8a64e", "My Function")]
+        public void ShouldTupleHashXOF128HashCorrectly(int testTupleId, int outputLength, string outputHex, string customization)
         {
             var tuple = GetTestTuple(testTupleId);
 
-            var expectedResult = new BitString(outputHex);
-            var hashFunction = GetTupleHashXOFFunction(digestSize, 256, customization);
+            var expectedResult = new BitString(outputHex, outputLength, false);
+            var hashFunction = GetTupleHashXOFFunction(outputLength, 256, customization);
 
             var subject = new TupleHash();
             var result = subject.HashMessage(hashFunction, tuple);
@@ -103,16 +105,16 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
         [TestCase(1, 256, "03ded4610ed6450a1e3f8bc44951d14fbc384ab0efe57b000df6b6df5aae7cd5", "")]
         [TestCase(1, 256, "6483cb3c9952eb20e830af4785851fc597ee3bf93bb7602c0ef6a65d741aeca7", "My Tuple App")]
         [TestCase(2, 256, "0c59b11464f2336c34663ed51b2b950bec743610856f36c28d1d088d8a244628", "My Tuple App")]
-        //[TestCase(1, 1607, "156408acc04508d50ed83fd67060a108061e93f85cb6b923cbccd76388d5e4e0441283fb31095c831ac4de13793286957385625a00f2e78b03e681f42f12f772935a5c4dc16e99b69ab9bfc65f7a6f30dc7e4d723ce6eda552b116d6fb33da4c976bd9616c0f97cafc338e2c5a50b7692c95f65038446f83670846a70c5ad51a242db3e53922024f85d39352cc67cdac101c7ed7793c9b37a33d1027c628a682736fc268b5aeb1c8548b38099abf3a1e08c845246fe358d902842bff77dc1ae142aa8443c544aac124", "My Function")]
-        //[TestCase(1, 35, "745800a905", "My Function")]
-        //[TestCase(3, 256, "b95455cc71c4701f6e508c7d5a049d5baf192987b5f4e07e6b7d9944ff388997", "My Function")]
-        //[TestCase(4, 256, "d763974ed5328c7b1bd00fb02c5480a90a5945b95e98b90f14b5581aecd0a426", "My Function")]
-        public void ShouldTupleHashXOF256HashCorrectly(int testTupleId, int digestSize, string outputHex, string customization)
+        [TestCase(1, 1607, "f292449dae5bbde3eaefe6ddc3fe657c317530cc7072ba451d3a844060b8517ebad72f6048fd93cc284d5a6f0d55c2aac444c11cd3ec10ed382dba2c367de9b6a5b371e8e108f1d1cc2b8462f4253e35b6f3bfd1f7c551e9aac6f2676c325a24edacf5f2db604446b1e5a3446f8b5f1f434c8fa6a3d69ff298a5988321be9420e666701de14e5d0e871a7c8c1ca4f5072209182119eda7b709db96b317803b4c91e4875271ef00d46bb365d8138353b6c5b3580ce5711f9983fcbfb2dc9c7ef00bcd2803555399392a", "My Function")]
+        [TestCase(1, 35, "f292449d06", "My Function")]
+        [TestCase(3, 256, "e56e3fd886eb48f61d304f4fbe3b505c12b20c22fd728e08a43cb863598dff0e", "My Function")]
+        [TestCase(4, 256, "4d1c388652ec4f07c552518dd1cbeecf081554aca4800c6a80f0302cecaa0527", "My Function")]
+        public void ShouldTupleHashXOF256HashCorrectly(int testTupleId, int outputLength, string outputHex, string customization)
         {
             var tuple = GetTestTuple(testTupleId);
 
-            var expectedResult = new BitString(outputHex);
-            var hashFunction = GetTupleHashXOFFunction(digestSize, 512, customization);
+            var expectedResult = new BitString(outputHex, outputLength, false);
+            var hashFunction = GetTupleHashXOFFunction(outputLength, 512, customization);
 
             var subject = new TupleHash();
             var result = subject.HashMessage(hashFunction, tuple);
@@ -159,6 +161,9 @@ namespace NIST.CVP.Crypto.TupleHash.Tests
                     break;
                 case 4:
                     array = new BitString[] { new BitString(""), new BitString("1011121314151617"), new BitString(""), new BitString(""), new BitString(""), new BitString(""), new BitString("") };
+                    break;
+                case 5:
+                    array = new BitString[] { new BitString("000102", 17, false), new BitString("1011121314", 35, false) };
                     break;
             } 
             return new List<BitString>(array);
