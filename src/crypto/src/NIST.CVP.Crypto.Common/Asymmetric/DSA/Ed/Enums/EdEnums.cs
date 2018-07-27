c3fbeff06b578d72736bc6609c4db113aa16029b
Matt Watson <mpw4@NIST.GOV>
2018-07-27 10:27:16 -0400

    Starts adding Edwards Curves

---
+++ src/crypto/src/NIST.CVP.Crypto.Common/Asymmetric/DSA/Ed/Enums/EdEnums.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace NIST.CVP.Crypto.Common.Asymmetric.DSA.Ed.Enums
+{
+    public enum Curve
+    {
+        [EnumMember(Value = "Edwards25519")]
+        Ed25519,
+        [EnumMember(Value = "Edwards448")]
+        Ed448
+    }
+
+    public enum CurveType
+    {
+        Prime,
+        Binary
+    }
+
+    public enum SecretGenerationMode
+    {
+        [EnumMember(Value = "testing candidates")]
+        TestingCandidates,
+
+        [EnumMember(Value = "extra bits")]
+        ExtraRandomBits
+    }
+}
