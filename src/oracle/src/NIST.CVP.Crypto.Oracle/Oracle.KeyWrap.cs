946d6da3c3cfa7b892be7ae198388074de815d40
Russ Hammett <russell.hammett@nist.gov>
2018-09-05 09:16:14 -0400

    KeyWrap Orleans grain impl

--- src/oracle/src/NIST.CVP.Crypto.Oracle/Oracle.KeyWrap.cs
+++ src/oracle/src/NIST.CVP.Crypto.Oracle/Oracle.KeyWrap.cs
@@ -2,49 +2,29 @@
 using System.Threading.Tasks;
 using NIST.CVP.Common.Oracle.ParameterTypes;
 using NIST.CVP.Common.Oracle.ResultTypes;
-using NIST.CVP.Crypto.KeyWrap;
+using NIST.CVP.Orleans.Grains.Interfaces;
+using NIST.CVP.Orleans.Grains.Interfaces.Helpers;
+using NIST.CVP.Orleans.Grains.Interfaces.KeyWrap;
 
 namespace NIST.CVP.Crypto.Oracle
 {
     public partial class Oracle
     {
-        private readonly KeyWrapFactory _keyWrapFactory = new KeyWrapFactory();
-
-        private KeyWrapResult GetKeyWrapCase(KeyWrapParameters param)
-        {
-            var keyWrap = _keyWrapFactory.GetKeyWrapInstance(param.KeyWrapType);
-
-            var key = _rand.GetRandomBitString(param.KeyLength);
-            var payload = _rand.GetRandomBitString(param.DataLength);
-
-            var result = new KeyWrapResult()
-            {
-                Key = key,
-                Plaintext = payload,
-                Ciphertext = keyWrap.Encrypt(key, payload, param.WithInverseCipher).Result,
-                TestPassed = true
-            };
-
-            if (param.CouldFail)
-            {
-                // Should Fail at certain ratio, 20%
-                var upperBound = (int)(1.0 / KEYWRAP_FAIL_RATIO);
-                var shouldFail = _rand.GetRandomInt(0, upperBound) == 0;
-
-                if (shouldFail)
-                {
-                    result.Ciphertext = _rand.GetDifferentBitStringOfSameSize(result.Ciphertext);
-                    result.TestPassed = false;
-                }
-            }
-
-            return result;
-        }
-
-
         public async Task<KeyWrapResult> GetKeyWrapCaseAsync(KeyWrapParameters param)
         {
-            return await _taskFactory.StartNew(() => GetKeyWrapCase(param));
+            var grain = _clusterClient.GetGrain<IOracleObserverKeyWrapCaseGrain>(
+                Guid.NewGuid()
+            );
+
+            var observer = new OracleGrainObserver<KeyWrapResult>();
+            var observerReference = 
+                await _clusterClient.CreateObjectReference<IGrainObserver<KeyWrapResult>>(observer);
+            await grain.Subscribe(observerReference);
+            await grain.BeginWorkAsync(param);
+
+            var result = await ObservableHelpers.ObserveUntilResult(grain, observer, observerReference);
+
+            return result;
         }
     }
 }
