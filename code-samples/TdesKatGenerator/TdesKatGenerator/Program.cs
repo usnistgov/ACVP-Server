cf45ef4c663461a46b20c9032fefac102b870804
ctc <christopher.celi@nist.gov>
2018-09-07 11:49:51 -0400

    Updates program to use real data

--- code-samples/TdesKatGenerator/TdesKatGenerator/Program.cs
+++ code-samples/TdesKatGenerator/TdesKatGenerator/Program.cs
@@ -1,5 +1,9 @@
 ﻿using CsvHelper;
+using NIST.CVP.Crypto.Common.Symmetric.BlockModes;
+using NIST.CVP.Crypto.Common.Symmetric.Enums;
 using NIST.CVP.Crypto.Common.Symmetric.TDES;
+using NIST.CVP.Crypto.Symmetric.BlockModes;
+using NIST.CVP.Crypto.Symmetric.Engines;
 using NIST.CVP.Math;
 using System.Collections.Generic;
 using System.IO;
@@ -12,29 +16,38 @@ namespace TdesKatGenerator
         {
             var original = new AlgoArrayResponse
             {
-                PlainText = new BitString("AB"),
-                CipherText = new BitString("CD"),
-                IV = new BitString("00"),
-                Key1 = BitString.Ones(64),
-                Key2 = BitString.Zeroes(64),
-                Key3 = BitString.Ones(64)
+                PlainText = BitString.Zeroes(64),
+                IV = BitString.Zeroes(64),
+                Key1 = new BitString("0101010101010101").ToOddParityBitString(),
+                Key2 = new BitString("0202020202020202").ToOddParityBitString(),
+                Key3 = new BitString("0303030303030303").ToOddParityBitString()
             };
 
             var kats = new List<AlgoArrayResponse>();
+            var ecb = new EcbBlockCipher(new TdesEngine());
 
-            var writer = new StreamWriter(@"C:\Users\ctc\Documents\file.csv");
+            var writer = new StreamWriter(@"C:\Users\ctc\Documents\variable-text.csv");
             var csv = new CsvWriter(writer);
             csv.Configuration.TypeConverterCache.AddConverter<BitString>(new BitStringConverter());
 
-            var flippedKeys = FlipEachBit(original.Keys);
-            flippedKeys.ForEach(fe =>
+            var flipped = FlipEachBit(original.PlainText);
+            flipped.ForEach(fe =>
             {
+                var param = new ModeBlockCipherParameters(
+                    BlockCipherDirections.Encrypt,
+                    original.IV,
+                    original.Keys,
+                    fe
+                );
+
+                var result = ecb.ProcessPayload(param);
+
                 kats.Add(new AlgoArrayResponse
                 {
-                    Keys = fe,
-                    PlainText = original.PlainText,
-                    CipherText = original.CipherText,
-                    IV = original.IV
+                    Keys = original.Keys,
+                    PlainText = fe,
+                    IV = original.IV,
+                    CipherText = result.Result
                 });
             });
 
