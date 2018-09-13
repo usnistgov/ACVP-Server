25724957e47124c0c7c135c27eada6e9ae7640d8
Russ Hammett <russell.hammett@nist.gov>
2018-09-13 13:41:50 -0400

    Environment -> EnvironmentConfig

---
+++ src/common/src/NIST.CVP.Common/Config/EnvironmentConfig.cs
@@ -0,0 +1,15 @@
+﻿using NIST.CVP.Common.Enums;
+
+namespace NIST.CVP.Common.Config
+{
+    /// <summary>
+    /// Environment config
+    /// </summary>
+    public class EnvironmentConfig
+    {
+        /// <summary>
+        /// The name of the environment
+        /// </summary>
+        public Environments Name { get; set; }
+    }
+}
---
+++ src/common/src/NIST.CVP.Common/Enums/Environments.cs
@@ -0,0 +1,11 @@
+﻿namespace NIST.CVP.Common.Enums
+{
+    public enum Environments
+    {
+        Local,
+        Dev,
+        Test,
+        Demo,
+        Prod
+    }
+}
