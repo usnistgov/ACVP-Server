da618adb2227438dcea69c7d857548c913f0a4c7
Russ Hammett <russell.hammett@nist.gov>
2018-09-26 14:22:01 -0400

    Consistency update - curve enums (ECDSA/EDDSA) to use capital letters.

--- src/crypto/src/NIST.CVP.Crypto.Common/Asymmetric/DSA/ECC/Enums/EccEnums.cs
+++ src/crypto/src/NIST.CVP.Crypto.Common/Asymmetric/DSA/ECC/Enums/EccEnums.cs
@@ -5,35 +5,35 @@ namespace NIST.CVP.Crypto.Common.Asymmetric.DSA.ECC.Enums
 {
     public enum Curve
     {
-        [EnumMember(Value = "p-192")]
+        [EnumMember(Value = "P-192")]
         P192,
-        [EnumMember(Value = "p-224")]
+        [EnumMember(Value = "P-224")]
         P224,
-        [EnumMember(Value = "p-256")]
+        [EnumMember(Value = "P-256")]
         P256,
-        [EnumMember(Value = "p-384")]
+        [EnumMember(Value = "P-384")]
         P384,
-        [EnumMember(Value = "p-521")]
+        [EnumMember(Value = "P-521")]
         P521,       // Should be 521 not 512.
-        [EnumMember(Value = "k-163")]
+        [EnumMember(Value = "K-163")]
         K163,
-        [EnumMember(Value = "k-233")]
+        [EnumMember(Value = "K-233")]
         K233,
-        [EnumMember(Value = "k-283")]
+        [EnumMember(Value = "K-283")]
         K283,
-        [EnumMember(Value = "k-409")]
+        [EnumMember(Value = "K-409")]
         K409,
-        [EnumMember(Value = "k-571")]
+        [EnumMember(Value = "K-571")]
         K571,
-        [EnumMember(Value = "b-163")]
+        [EnumMember(Value = "B-163")]
         B163,
-        [EnumMember(Value = "b-233")]
+        [EnumMember(Value = "B-233")]
         B233,
-        [EnumMember(Value = "b-283")]
+        [EnumMember(Value = "B-283")]
         B283,
-        [EnumMember(Value = "b-409")]
+        [EnumMember(Value = "B-409")]
         B409,
-        [EnumMember(Value = "b-571")]
+        [EnumMember(Value = "B-571")]
         B571
     }
 
