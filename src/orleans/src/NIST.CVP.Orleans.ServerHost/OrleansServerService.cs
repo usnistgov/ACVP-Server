67cdeeee494ef08274bccefd567fa0d5a78ac0ff
ctc <christopher.celi@nist.gov>
2018-10-09 14:26:01 -0400

    Updates Orleans Server to build into a service (but doesn't run)

---
+++ src/orleans/src/NIST.CVP.Orleans.ServerHost/OrleansServerService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NIST.CVP.Common.Config;
+using NIST.CVP.Common.Helpers;
+using NIST.CVP.Orleans.Grains;
+using NIST.CVP.Orleans.Grains.Interfaces;
+using NIST.CVP.Orleans.ServerHost.ExtensionMethods;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.ServiceProcess;
+
+namespace NIST.CVP.Orleans.ServerHost
+{
+    public class OrleansServerService : ServiceBase
+    {
+        private static IServiceProvider ServiceProvider { get; }
+        private static readonly string RootDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private static readonly OrleansConfig OrleansConfig;
+        private static readonly EnvironmentConfig EnvironmentConfig;
+        private static ISiloHost _silo;
+
+        static OrleansServerService()
+        {
+            ServiceProvider = EntryPointConfigHelper.Bootstrap(RootDirectory);
+            OrleansConfig = ServiceProvider.GetService<IOptions<OrleansConfig>>().Value;
+            EnvironmentConfig = ServiceProvider.GetService<IOptions<EnvironmentConfig>>().Value;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+
+            var builder = new SiloHostBuilder()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = OrleansConfig.ClusterId;
+                    options.ServiceId = Constants.ServiceId;
+                })
+                .ConfigureServices(Grains.ConfigureServices.RegisterServices)
+                .ConfigureApplicationParts(parts =>
+                {
+                    parts.AddApplicationPart(typeof(IGrainMarker).Assembly).WithReferences();
+                })
+                .ConfigureClustering(OrleansConfig, EnvironmentConfig)
+                .ConfigureLogging(OrleansConfig, EnvironmentConfig)
+                .UseDashboard(options => { });
+
+            _silo = builder.Build();
+            _silo.StartAsync().Wait();
+        }
+
+        protected override void OnStop()
+        {
+            _silo.StopAsync().Wait();
+        }
+    }
+}
--- src/orleans/src/NIST.CVP.Orleans.ServerHost/Program.cs
+++ src/orleans/src/NIST.CVP.Orleans.ServerHost/Program.cs
@@ -1,56 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using NIST.CVP.Common.Config;
-using NIST.CVP.Common.Helpers;
-using NIST.CVP.Orleans.Grains;
-using NIST.CVP.Orleans.Grains.Interfaces;
-using NIST.CVP.Orleans.ServerHost.ExtensionMethods;
-using Orleans;
-using Orleans.Configuration;
-using Orleans.Hosting;
-using System;
+﻿using System.ServiceProcess;
 
 namespace NIST.CVP.Orleans.ServerHost
 {
     public static class Program
     {
-        private static IServiceProvider ServiceProvider { get; }
-        private static readonly string RootDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        private static readonly OrleansConfig OrleansConfig;
-        private static readonly EnvironmentConfig EnvironmentConfig;
-
-        static Program()
+        public static void Main(string[] args)
         {
-            ServiceProvider = EntryPointConfigHelper.Bootstrap(RootDirectory);
-            OrleansConfig = ServiceProvider.GetService<IOptions<OrleansConfig>>().Value;
-            EnvironmentConfig = ServiceProvider.GetService<IOptions<EnvironmentConfig>>().Value;
-        }
-        
-        static void Main(string[] args)
-        {
-            var builder = new SiloHostBuilder()
-                .Configure<ClusterOptions>(options =>
-                {
-                    options.ClusterId = OrleansConfig.ClusterId;
-                    options.ServiceId = Constants.ServiceId;
-                })
-                .ConfigureServices(ConfigureServices.RegisterServices)
-                .ConfigureApplicationParts(parts =>
-                {
-                    parts.AddApplicationPart(typeof(IGrainMarker).Assembly).WithReferences();
-                })
-                .ConfigureClustering(OrleansConfig, EnvironmentConfig)
-                .ConfigureLogging(OrleansConfig, EnvironmentConfig)
-                .UseDashboard(options => { });
-
-            var silo = builder.Build();
-            silo.StartAsync().Wait();
-
-            Console.WriteLine("Press Enter to close.");
-            Console.ReadLine();
-
-            // shut the silo down after we are done.
-            silo.StopAsync().Wait();
+            ServiceBase.Run(new OrleansServerService());
         }
     }
 }
