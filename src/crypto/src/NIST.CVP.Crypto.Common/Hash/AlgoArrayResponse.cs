b899d1673f4bfab0becaa4eb6ffd6321cb2dcbeb
Russ Hammett <russell.hammett@nist.gov>
2018-10-10 15:52:31 -0400

    JsonProperty on sha algoarrayresponse

--- src/crypto/src/NIST.CVP.Crypto.Common/Hash/AlgoArrayResponse.cs
+++ src/crypto/src/NIST.CVP.Crypto.Common/Hash/AlgoArrayResponse.cs
@@ -1,10 +1,13 @@
-﻿using NIST.CVP.Math;
+﻿using Newtonsoft.Json;
+using NIST.CVP.Math;
 
 namespace NIST.CVP.Crypto.Common.Hash
 {
     public class AlgoArrayResponse
     {
+        [JsonProperty(PropertyName = "msg")]
         public BitString Message { get; set; }
+        [JsonProperty(PropertyName = "md")]
         public BitString Digest { get; set; }
         public int DigestLength => Digest.BitLength;
     }
