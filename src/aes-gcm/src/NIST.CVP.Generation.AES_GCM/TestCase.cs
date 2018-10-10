c8fc99798f9eee4238d92aef46ca5a8ed85f940c
Russ Hammett <russell.hammett@nist.gov>
2018-10-10 11:21:02 -0400

    AES-GCM updates to match specification

--- src/aes-gcm/src/NIST.CVP.Generation.AES_GCM/TestCase.cs
+++ src/aes-gcm/src/NIST.CVP.Generation.AES_GCM/TestCase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NIST.CVP.Generation.Core;
 using NIST.CVP.Generation.Core.ExtensionMethods;
@@ -14,11 +15,17 @@ namespace NIST.CVP.Generation.AES_GCM
         public bool? TestPassed { get; set; }
         public bool Deferred { get; set; }
         public TestGroup ParentGroup { get; set; }
+        [JsonProperty(PropertyName = "pt")]
         public BitString PlainText { get; set; }
+        [JsonProperty(PropertyName = "key")]
         public BitString Key { get; set; }
+        [JsonProperty(PropertyName = "aad")]
         public BitString AAD { get; set; }
+        [JsonProperty(PropertyName = "iv")]
         public BitString IV { get; set; }
+        [JsonProperty(PropertyName = "ct")]
         public BitString CipherText { get; set; }
+        [JsonProperty(PropertyName = "tag")]
         public BitString Tag { get; set; }
 
         public bool SetString(string name, string value)
--- src/aes-gcm/src/NIST.CVP.Generation.AES_GCM/TestGroup.cs
+++ src/aes-gcm/src/NIST.CVP.Generation.AES_GCM/TestGroup.cs
@@ -11,7 +11,8 @@ namespace NIST.CVP.Generation.AES_GCM
     public class TestGroup : ITestGroup<TestGroup, TestCase>
     {
         public int TestGroupId { get; set; }
-        public string TestType { get; set; }
+        public string TestType { get; set; } = "AFT";
+        public string InternalTestType { get; set; } = "AFT";
         [JsonProperty(PropertyName = "direction")]
         public string Function { get; set; }
         [JsonProperty(PropertyName = "keyLen")]
