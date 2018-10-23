a61f76d0bb92490b673ffea49b504ed4a7754d59
Kritner <russell.hammett@nist.gov>
2018-10-23 11:01:49 -0400

    Updates SHA1/2 to output formatted algorithm name for IUT use

--- src/crypto/src/NIST.CVP.Crypto.Common/Hash/SHA2/DigestSizes.cs
+++ src/crypto/src/NIST.CVP.Crypto.Common/Hash/SHA2/DigestSizes.cs
@@ -1,14 +1,24 @@
-﻿namespace NIST.CVP.Crypto.Common.Hash.SHA2
+﻿using System.Runtime.Serialization;
+
+namespace NIST.CVP.Crypto.Common.Hash.SHA2
 {
     public enum DigestSizes
     {
+        [EnumMember(Value = "160")]
         d160,
+        [EnumMember(Value = "224")]
         d224,
+        [EnumMember(Value = "256")]
         d256,
+        [EnumMember(Value = "384")]
         d384,
+        [EnumMember(Value = "512")]
         d512,
+        [EnumMember(Value = "512/224")]
         d512t224,
+        [EnumMember(Value = "512/256")]
         d512t256,
+        [EnumMember(Value = "NONE")]
         NONE
     }
 }
