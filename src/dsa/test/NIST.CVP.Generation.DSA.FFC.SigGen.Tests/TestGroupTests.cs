afaa4f14c332a204e1f693f2d20a454fbbd6dd65
Kritner <russell.hammett@nist.gov>
2018-11-15 10:41:16 -0500

    Updates several EC/ED/DSA groups to handle isSample properly for projection - adds tests

---
+++ src/dsa/test/NIST.CVP.Generation.DSA.FFC.SigGen.Tests/TestGroupTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using NIST.CVP.Crypto.Common.Asymmetric.DSA.FFC;
+using NIST.CVP.Tests.Core.TestCategoryAttributes;
+using NUnit.Framework;
+
+namespace NIST.CVP.Generation.DSA.FFC.SigGen.Tests
+{
+    [TestFixture, UnitTest]
+    public class TestGroupTests
+    {
+        private TestGroup _subject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subject = new TestGroup();
+        }
+
+        [Test]
+        public void KeyPropertiesReturnZeroWhenUnderlyingPropertyIsNull()
+        {
+            var checkValue = new BigInteger(0);
+
+            _subject.DomainParams = null;
+
+            Assert.AreEqual(checkValue, _subject.P, nameof(_subject.P));
+            Assert.AreEqual(checkValue, _subject.Q, nameof(_subject.Q));
+            Assert.AreEqual(checkValue, _subject.G, nameof(_subject.G));
+        }
+
+        [Test]
+        public void KeyPropertiesReturnActualValueWhenProvided()
+        {
+            var p = new BigInteger(1);
+            var q = new BigInteger(2);
+            var g = new BigInteger(3);
+            _subject.DomainParams = new FfcDomainParameters(p, q, g);
+
+            Assert.AreEqual(p, _subject.P, nameof(_subject.P));
+            Assert.AreEqual(q, _subject.Q, nameof(_subject.Q));
+            Assert.AreEqual(g, _subject.G, nameof(_subject.G));
+        }
+    }
+}
---
+++ src/ecdsa/test/NIST.CVP.Generation.DSA.ECC.SigGen.Tests/TestGroupTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using NIST.CVP.Crypto.Common.Asymmetric.DSA.ECC;
+using NIST.CVP.Tests.Core.TestCategoryAttributes;
+using NUnit.Framework;
+
+namespace NIST.CVP.Generation.DSA.ECC.SigGen.Tests
+{
+    [TestFixture, UnitTest]
+    public class TestGroupTests
+    {
+        private TestGroup _subject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subject = new TestGroup();
+        }
+
+        [Test]
+        public void KeyPropertiesReturnZeroWhenUnderlyingPropertyIsNull()
+        {
+            var checkValue = new BigInteger(0);
+
+            _subject.KeyPair = null;
+
+            Assert.AreEqual(checkValue, _subject.D, nameof(_subject.D));
+            Assert.AreEqual(checkValue, _subject.Qx, nameof(_subject.Qx));
+            Assert.AreEqual(checkValue, _subject.Qy, nameof(_subject.Qy));
+        }
+
+        [Test]
+        public void KeyPropertiesReturnActualValueWhenProvided()
+        {
+            var d = new BigInteger(1);
+            var qx = new BigInteger(2);
+            var qy = new BigInteger(3);
+            _subject.KeyPair = new EccKeyPair(new EccPoint(qx, qy), d);
+
+            Assert.AreEqual(d, _subject.D, nameof(_subject.D));
+            Assert.AreEqual(qx, _subject.Qx, nameof(_subject.Qx));
+            Assert.AreEqual(qy, _subject.Qy, nameof(_subject.Qy));
+        }
+    }
+}
