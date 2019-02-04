c58855e8cdf728456b6b5faf48914a9412af6097
Russ Hammett <russell.hammett@nist.gov>
2019-02-04 14:01:22 -0500

    Hello world in mongoDb

---
+++ code-samples/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost");
+            var server = client.GetDatabase("myHelloWorld");
+
+            var collection = server.GetCollection<SomeObject>("SomeObjects");
+            Console.WriteLine($"Count: {collection.CountDocuments(c => !string.IsNullOrEmpty(c.Message))}");
+
+            collection.InsertOne(new SomeObject()
+            {
+                ObjectCreation = DateTime.Now,
+                Message = "Hello World!"
+            });
+
+            Console.WriteLine($"Count: {collection.CountDocuments(c => !string.IsNullOrEmpty(c.Message))}");
+        }
+    }
+}
---
+++ code-samples/ConsoleApp1/SomeObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SomeObject
+    {
+        public DateTime ObjectCreation { get; set; }
+        public string Message { get; set; }
+    }
+}
