a1a8b0534ea50f7189e6825b5058d0afbb86bc06
Russ Hammett <russell.hammett@nist.gov>
2019-02-07 12:18:58 -0500

    Adds SSDT project for sql objects related to AcvpPools

---
+++ src/common/src/NIST.CVP.SQL.AcvpPools/dbo/Stored Procedures/GetPoolValue.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Russ Hammett
+-- Create date: 2019-02-06
+-- Description:	Gets a value from the specified pool
+-- =============================================
+CREATE PROCEDURE [dbo].[GetPoolValue]
+	@poolName VARCHAR(512)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @poolId BIGINT
+	EXEC [dbo].[GetPoolIdFromName] @poolName, @poolId OUTPUT
+
+    DELETE TOP(1) [dbo].[PoolValues] WITH (READPAST)
+	OUTPUT deleted.*  
+	WHERE poolId = @poolId
+		and isStagingValue = 0
+
+END
