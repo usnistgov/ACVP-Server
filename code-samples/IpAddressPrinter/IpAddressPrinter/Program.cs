dfbe83cadda55c18f3ebc5a11722db1be435d154
Hammett <rlh4@NIST.GOV>
2019-03-18 08:15:11 -0400

    Ip address printer utility

---
+++ code-samples/IpAddressPrinter/IpAddressPrinter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IpAddressPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Your IP address is \"{GetLocalIpAddress()}\"");
+        }
+
+        private static string GetLocalIpAddress()
+        {
+            try
+            {
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+                {
+                    socket.Connect("8.8.8.8", 65530);
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    return endPoint?.Address.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
