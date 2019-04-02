1eb98dd585fa33644f1ce15c09bf41bfece9218a
Russell Hammett <russell.hammett@nist.gov>
2019-04-02 13:35:38 -0400

    ECDSA revision

--- src/ecdsa/src/NIST.CVP.Generation.DSA.ECC.SigVer/TestVectorSet.cs
+++ src/ecdsa/src/NIST.CVP.Generation.DSA.ECC.SigVer/TestVectorSet.cs
@@ -13,6 +13,7 @@ namespace NIST.CVP.Generation.DSA.ECC.SigVer
         public int VectorSetId { get; set; }
         public string Algorithm { get; set; } = "ECDSA";
         public string Mode { get; set; } = "SigVer";
+        public string Revision { get; set; }
         public bool IsSample { get; set; }
 
         public List<TestGroup> TestGroups { get; set; } = new List<TestGroup>();
