5c5dace4acf1204f3afbb537f3855a824f353176
Russell Hammett <russell.hammett@nist.gov>
2019-04-02 11:45:16 -0400

    CSHAKE revision

--- src/cshake/src/NIST.CVP.Generation.CSHAKE/Parameters.cs
+++ src/cshake/src/NIST.CVP.Generation.CSHAKE/Parameters.cs
@@ -13,6 +13,7 @@ namespace NIST.CVP.Generation.CSHAKE
         public int VectorSetId { get; set; }
         public string Algorithm { get; set; }
         public string Mode { get; set; }
+        public string Revision { get; set; }
         public bool IsSample { get; set; }
         public string[] Conformances { get; set; } = { };
         
