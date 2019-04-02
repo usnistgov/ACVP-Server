96449163bdc13449f5d8612b6a8a1f2835bfb13d
Russell Hammett <russell.hammett@nist.gov>
2019-04-02 16:09:02 -0400

    KMAC revision

--- src/kmac/src/NIST.CVP.Generation.KMAC/Parameters.cs
+++ src/kmac/src/NIST.CVP.Generation.KMAC/Parameters.cs
@@ -9,6 +9,7 @@ namespace NIST.CVP.Generation.KMAC
         public int VectorSetId { get; set; }
         public string Algorithm { get; set; }
         public string Mode { get; set; }
+        public string Revision { get; set; }
         public bool IsSample { get; set; }
         public string[] Conformances { get; set; } = { };
 
