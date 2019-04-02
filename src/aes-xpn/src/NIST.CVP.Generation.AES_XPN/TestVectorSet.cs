91124e32923197a087c47e7dc134281ad3b62b07
Russell Hammett <russell.hammett@nist.gov>
2019-04-02 10:50:59 -0400

    AES-XPN revision

--- src/aes-xpn/src/NIST.CVP.Generation.AES_XPN/TestVectorSet.cs
+++ src/aes-xpn/src/NIST.CVP.Generation.AES_XPN/TestVectorSet.cs
@@ -14,6 +14,7 @@ namespace NIST.CVP.Generation.AES_XPN
         public int VectorSetId { get; set; }
         public string Algorithm { get; set; } = "AES";
         public string Mode { get; set; } = "XPN";
+        public string Revision { get; set; }
         public bool IsSample { get; set; }
         public List<TestGroup> TestGroups { get; set; } = new List<TestGroup>();
     }
