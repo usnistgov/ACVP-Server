f8be0880f78dfbf97242340a4a59076ae7f164e4
Kritner <russell.hammett@nist.gov>
2019-04-22 15:05:53 -0400

    Begin setting up abstractions for ciphertext stealin transforms

---
+++ src/crypto/src/NIST.CVP.Crypto.Common/Symmetric/CTS/ICiphertextStealingTransform.cs
@@ -0,0 +1,12 @@
+﻿using NIST.CVP.Math;
+
+namespace NIST.CVP.Crypto.Common.Symmetric.CTS
+{
+    /// <summary>
+    /// Used to apply a ciphertext stealing mode's final block(s) transform.
+    /// </summary>
+    public interface ICiphertextStealingTransform
+    {
+        BitString Transform(BitString ciphertext);
+    }
+}
---
+++ src/crypto/src/NIST.CVP.Crypto.Common/Symmetric/CTS/ICiphertextStealingTransformFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace NIST.CVP.Crypto.Common.Symmetric.CTS
+{
+    public interface ICiphertextStealingTransformFactory
+    {
+        ICiphertextStealingTransform Get(CiphertextStealingMode mode);
+    }
+
+    /// <summary>
+    /// The differing modes of ciphertext stealing
+    /// </summary>
+    public enum CiphertextStealingMode
+    {
+        /// <summary>
+        /// The ciphertext is not switched, returned as is.
+        /// </summary>
+        CS1,
+        /// <summary>
+        /// Switches the last two blocks of ciphertext conditionally. The blocks are switched only when the last block is not a completely block.
+        /// </summary>
+        CS2,
+        /// <summary>
+        /// Always switches last two blocks of ciphertext
+        /// </summary>
+        CS3
+    }
+}
