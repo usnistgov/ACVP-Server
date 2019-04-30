e5adf615c0183c9b7869ee1d5959f4b1c67fa8c0
Kritner <russell.hammett@nist.gov>
2019-04-30 11:27:53 -0400

    DSA.SigGen await correction

--- src/common/src/NIST.CVP.Generation/DSA/v1_0/SigGen/TestGroupGenerator.cs
+++ src/common/src/NIST.CVP.Generation/DSA/v1_0/SigGen/TestGroupGenerator.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using NIST.CVP.Common.Oracle;
+﻿using NIST.CVP.Common.Oracle;
 using NIST.CVP.Common.Oracle.ParameterTypes;
+using NIST.CVP.Common.Oracle.ResultTypes;
 using NIST.CVP.Crypto.Common.Asymmetric.DSA.FFC;
 using NIST.CVP.Crypto.Common.Asymmetric.DSA.FFC.Enums;
 using NIST.CVP.Crypto.Common.Hash.ShaWrapper.Helpers;
 using NIST.CVP.Generation.Core;
 using NLog;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NIST.CVP.Generation.DSA.v1_0.SigGen
 {
@@ -24,12 +23,43 @@ namespace NIST.CVP.Generation.DSA.v1_0.SigGen
         public IEnumerable<TestGroup> BuildTestGroups(Parameters parameters)
         {
             var groups = BuildTestGroupsAsync(parameters);
-            return groups.Result.ToArray();
+            groups.Wait();
+
+            return groups.Result;
         }
 
-        private async Task<IEnumerable<TestGroup>> BuildTestGroupsAsync(Parameters parameters)
+        private async Task<List<TestGroup>> BuildTestGroupsAsync(Parameters parameters)
         {
             var testGroups = new List<TestGroup>();
+
+            if (!parameters.IsSample)
+            {
+                foreach (var capability in parameters.Capabilities)
+                {
+                    var n = capability.N;
+                    var l = capability.L;
+
+                    foreach (var hashAlg in capability.HashAlg)
+                    {
+                        var hashFunction = ShaAttributes.GetHashFunctionFromName(hashAlg);
+
+                        var testGroup = new TestGroup
+                        {
+                            L = l,
+                            N = n,
+                            HashAlg = hashFunction
+                        };
+
+                        testGroups.Add(testGroup);
+                    }
+                }
+
+                return testGroups;
+            }
+
+            // For a sample, we need to generate everything up front (since the IUT would normally do this work)
+            Dictionary<TestGroup, Task<DsaDomainParametersResult>> mapDomainParam =
+                new Dictionary<TestGroup, Task<DsaDomainParametersResult>>();
 
             foreach (var capability in parameters.Capabilities)
             {
@@ -40,49 +70,56 @@ namespace NIST.CVP.Generation.DSA.v1_0.SigGen
                 {
                     var hashFunction = ShaAttributes.GetHashFunctionFromName(hashAlg);
 
-                    FfcDomainParameters domainParams = null;
-                    FfcKeyPair key = null;
-                    if (parameters.IsSample)
-                    {
-                        var param = new DsaDomainParametersParameters
-                        {
-                            HashAlg = hashFunction,
-                            PQGenMode = PrimeGenMode.Provable,
-                            GGenMode = GeneratorGenMode.Unverifiable,
-                            L = l,
-                            N = n
-                        };
-
-                        try
-                        {
-                            var domainParamResult = await _oracle.GetDsaDomainParametersAsync(param);
-                            domainParams = new FfcDomainParameters(domainParamResult.P, domainParamResult.Q, domainParamResult.G);
-
-                            var keyResult = await _oracle.GetDsaKeyAsync(
-                                new DsaKeyParameters()
-                                {
-                                    DomainParameters = domainParams
-                                });
-                            key = keyResult.Key;
-                        }
-                        catch (Exception ex)
-                        {
-                            ThisLogger.Error(ex);
-                            throw;
-                        }
-                    }
-
                     var testGroup = new TestGroup
                     {
                         L = l,
                         N = n,
-                        HashAlg = hashFunction,
-                        DomainParams = domainParams,
-                        Key = key
+                        HashAlg = hashFunction
                     };
 
-                    testGroups.Add(testGroup);
+                    var param = new DsaDomainParametersParameters
+                    {
+                        HashAlg = hashFunction,
+                        PQGenMode = PrimeGenMode.Provable,
+                        GGenMode = GeneratorGenMode.Unverifiable,
+                        L = l,
+                        N = n
+                    };
+
+                    mapDomainParam.Add(testGroup, _oracle.GetDsaDomainParametersAsync(param));
                 }
+            }
+
+            await Task.WhenAll(mapDomainParam.Values);
+
+            Dictionary<TestGroup, Task<DsaKeyResult>> mapKey =
+                new Dictionary<TestGroup, Task<DsaKeyResult>>();
+            foreach (var keyValuePair in mapDomainParam)
+            {
+                var group = keyValuePair.Key;
+                var domainParam = keyValuePair.Value.Result;
+                group.DomainParams = new FfcDomainParameters()
+                {
+                    G = domainParam.G,
+                    P = domainParam.P,
+                    Q = domainParam.Q
+                };
+
+                mapKey.Add(group, _oracle.GetDsaKeyAsync(new DsaKeyParameters()
+                {
+                    DomainParameters = group.DomainParams
+                }));
+            }
+
+            await Task.WhenAll(mapKey.Values);
+
+            foreach (var keyValuePair in mapKey)
+            {
+                var group = keyValuePair.Key;
+                var key = keyValuePair.Value.Result;
+                group.Key = key.Key;
+
+                testGroups.Add(group);
             }
 
             return testGroups;
