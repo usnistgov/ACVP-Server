c682f5235b36a699283be95b72afa39ec1580a14
Kritner <russell.hammett@nist.gov>
2019-05-06 14:52:35 -0400

    SP800-106 Randomizer/padding function crypto and tests - https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-106.pdf

---
+++ src/crypto/src/NIST.CVP.Crypto/SP800-106/PreSigVerMessageRandomizerBuilder.cs
@@ -0,0 +1,27 @@
+﻿using NIST.CVP.Crypto.Common;
+using NIST.CVP.Crypto.Common.Asymmetric;
+using NIST.CVP.Math.Entropy;
+
+namespace NIST.CVP.Crypto
+{
+    public class PreSigVerMessageRandomizerBuilder : IPreSigVerMessageRandomizerBuilder
+    {
+        private IEntropyProvider _entropyProvider;
+
+        public PreSigVerMessageRandomizerBuilder(IEntropyProviderFactory entropyProviderFactory)
+        {
+            _entropyProvider = entropyProviderFactory.GetEntropyProvider(EntropyProviderTypes.Random);
+        }
+
+        public IPreSigVerMessageRandomizerBuilder WithEntropyProvider(IEntropyProvider entropyProvider)
+        {
+            _entropyProvider = entropyProvider;
+            return this;
+        }
+
+        public IPreSigVerMessageRandomizer Build()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
