d25aff664d027a4eccda3d1d267219af7dbbf033
Russell Hammett <Kritner@gmail.com>
2019-07-31 11:47:42 -0400

    Minor updates to Kas and KasScheme abstraction - nothing substantial, just need to switch branches

--- src/crypto/src/NIST.CVP.Crypto.Common/KAS/IKasIfc.cs
+++ src/crypto/src/NIST.CVP.Crypto.Common/KAS/IKasIfc.cs
@@ -14,12 +14,6 @@ namespace NIST.CVP.Crypto.Common.KAS
         /// </summary>
         ISchemeIfc Scheme { get; }
         /// <summary>
-        /// Sets up this instance of KAS with the information from this party for contributing
-        /// to key establishment.
-        /// </summary>
-        /// <param name="secretKeyingMaterial">This party's secret keying material.</param>
-        void SetThisPartySecretKeyingMaterial(IIfcSecretKeyingMaterial secretKeyingMaterial);
-        /// <summary>
         /// Establish a key between this party and the other party.
         /// </summary>
         /// <param name="otherPartyKeyingMaterial">The other parties keying material that contributes to the key.</param>
--- src/crypto/src/NIST.CVP.Crypto.Common/KAS/Scheme/ISchemeIfc.cs
+++ src/crypto/src/NIST.CVP.Crypto.Common/KAS/Scheme/ISchemeIfc.cs
@@ -2,6 +2,23 @@ namespace NIST.CVP.Crypto.Common.KAS.Scheme
 {
     public interface ISchemeIfc
     {
+        /// <summary>
+        /// The length of the other input going into a kdf
+        /// </summary>
         int OtherInputLength { get; }
+        /// <summary>
+        /// The options for the scheme, such as this party's role in the KAS, whether key confirmation occurs, etc.
+        /// </summary>
+        SchemeParametersIfc SchemeParameters { get; }
+        /// <summary>
+        /// The keying material to be used by this party.  Can be a key pair or a nonce.
+        /// </summary>
+        IIfcSecretKeyingMaterial ThisPartyKeyingMaterial { get; }
+        /// <summary>
+        /// Computes the KAS result based on party u/v contributions to the key, as well as kas scheme options.
+        /// </summary>
+        /// <param name="otherPartyKeyingMaterial">The other parties key contributions.</param>
+        /// <returns>The result of the KAS operation.</returns>
+        KasResult ComputeResult(IIfcSecretKeyingMaterial otherPartyKeyingMaterial);
     }
 }
