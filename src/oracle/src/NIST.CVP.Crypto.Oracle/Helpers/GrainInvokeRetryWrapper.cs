dff2518af2da74417a04635f2003b0d57e838cb7
Hammett <rlh4@NIST.GOV>
2019-10-30 10:18:38 -0400

    Updates GrainInvokeRetryWrapper to account for timeout exceptions.

--- src/oracle/src/NIST.CVP.Crypto.Oracle/Helpers/GrainInvokeRetryWrapper.cs
+++ src/oracle/src/NIST.CVP.Crypto.Oracle/Helpers/GrainInvokeRetryWrapper.cs
@@ -3,73 +3,90 @@ using System;
 using System.Threading.Tasks;
 using NIST.CVP.Orleans.Grains.Interfaces;
 using NIST.CVP.Crypto.Oracle.Exceptions;
+using NLog;
 
 namespace NIST.CVP.Crypto.Oracle.Helpers
 {
     /// <summary>
-    /// Contains several generic methods that can be used to wrap grain calls in a recursive retry
-    /// due to GatewayTooBusyExceptions from the Orleans cluster.
+    /// Contains several generic methods that can be used to wrap grain calls in a while loop until x attempts attempted 
+    /// due to GatewayTooBusyExceptions/TimeoutExceptions from the Orleans cluster.
     ///
-    /// This exception is thrown by the cluster when LoadShedding is enabled and the cluster's CPU exceeds
+    /// This GatewayTooBusyExceptions are thrown by the cluster when LoadShedding is enabled and the cluster's CPU exceeds
     /// the threshold amount.
     /// </summary>
     public static class GrainInvokeRetryWrapper
     {
+        private static readonly ILogger ThisLogger = LogManager.GetCurrentClassLogger();
+        
         /// <summary>
         /// Wrap a grain call that returns a <see cref="Task"/> and takes a single parameter.
         /// </summary>
         /// <typeparam name="T">The parameter type.</typeparam>
         /// <param name="func">The function to invoke.</param>
         /// <param name="param">The parameter to use with the function invoke.</param>
-        /// <param name="timesToRetry">The number of times to retry the grain call's that failed due to LoadShedding</param>
+        /// <param name="timesToRetry">The number of times to retry the grain calls that failed due to LoadShedding or Timeout.</param>
         /// <returns>Task</returns>
+        /// <exception cref="OrleansGrainFailedAfterRetries">Thrown when too many failed attempts of invoking grains occurs.</exception>
         public static async Task WrapGrainCall<T>(Func<T, Task> func, T param, int timesToRetry)
         {
-            if (timesToRetry < 0)
+            while (timesToRetry > 0)
             {
-                throw new OrleansGrainFailedAfterRetries();
-            }
-
-            timesToRetry--;
-
-            try
-            {
-                await func(param);
-            }
-            catch (GatewayTooBusyException)
-            {
-                // Gateway too busy - wait 5 seconds and try again.
+                timesToRetry--;
+                
+                try
+                {
+                    await func(param);
+                    return;
+                }
+                catch (TimeoutException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(TimeoutException)} stack: {ex.StackTrace}");
+                }
+                catch (GatewayTooBusyException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(GatewayTooBusyException)} stack: {ex.StackTrace}");
+                }
+                
                 await Task.Delay(TimeSpan.FromSeconds(Constants.TaskPollingSeconds));
-                await WrapGrainCall(func, param, timesToRetry);
             }
+
+            ThisLogger.Error("Too many failed attempts at invoking a grain.");
+            throw new OrleansGrainFailedAfterRetries();
         }
 
         /// <summary>
-        /// Wrap a grain call that returns a <see cref="TResult"/> and no parameters.
+        /// Wrap a grain call with no parameters.
         /// </summary>
         /// <typeparam name="TResult">The result type.</typeparam>
         /// <param name="func">The function to invoke.</param>
-        /// <param name="timesToRetry">The number of times to retry the grain call's that failed due to LoadShedding</param>
+        /// <param name="timesToRetry">The number of times to retry the grain calls that failed due to LoadShedding or Timeout.</param>
         /// <returns>Task wrapped TResult</returns>
-        public static async Task<TResult> WrapGrainCall<TResult>(Func<Task<TResult>> func, int timesToRetry)
+        /// <exception cref="OrleansGrainFailedAfterRetries">Thrown when too many failed attempts of invoking grains occurs.</exception>
+        public static async Task WrapGrainCall<TResult>(Func<Task<TResult>> func, int timesToRetry)
         {
-            if (timesToRetry < 0)
+            while (timesToRetry > 0)
             {
-                throw new OrleansGrainFailedAfterRetries();
-            }
-
-            timesToRetry--;
-
-            try
-            {
-                return await func();
-            }
-            catch (GatewayTooBusyException)
-            {
-                // Gateway too busy - wait 5 seconds and try again.
+                timesToRetry--;
+                
+                try
+                {
+                    await func();
+                    return;
+                }
+                catch (TimeoutException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(TimeoutException)} stack: {ex.StackTrace}");
+                }
+                catch (GatewayTooBusyException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(GatewayTooBusyException)} stack: {ex.StackTrace}");
+                }
+                
                 await Task.Delay(TimeSpan.FromSeconds(Constants.TaskPollingSeconds));
-                return await WrapGrainCall(func, timesToRetry);
             }
+            
+            ThisLogger.Error("Too many failed attempts at invoking a grain.");
+            throw new OrleansGrainFailedAfterRetries();
         }
 
         /// <summary>
@@ -79,31 +96,37 @@ namespace NIST.CVP.Crypto.Oracle.Helpers
         /// <typeparam name="TResult">The result type.</typeparam>
         /// <param name="func">The function to invoke.</param>
         /// <param name="param">The parameter to use with the function invoke.</param>
-        /// <param name="timesToRetry">The number of times to retry the grain call's that failed due to LoadShedding</param>
+        /// <param name="timesToRetry">The number of times to retry the grain calls that failed due to LoadShedding or Timeout.</param>
         /// <returns>Task wrapped TResult</returns>
+        /// <exception cref="OrleansGrainFailedAfterRetries">Thrown when too many failed attempts of invoking grains occurs.</exception>
         public static async Task<TResult> WrapGrainCall<T, TResult>(Func<T, Task<TResult>> func, T param, int timesToRetry)
         {
-            if (timesToRetry < 0)
+            while (timesToRetry > 0)
             {
-                throw new OrleansGrainFailedAfterRetries();
-            }
-
-            timesToRetry--;
-
-            try
-            {
-                return await func(param);
-            }
-            catch (GatewayTooBusyException)
-            {
-                // Gateway too busy - wait 5 seconds and try again.
+                timesToRetry--;
+                
+                try
+                {
+                    return await func(param);
+                }
+                catch (TimeoutException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(TimeoutException)} stack: {ex.StackTrace}");
+                }
+                catch (GatewayTooBusyException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(GatewayTooBusyException)} stack: {ex.StackTrace}");
+                }
+                
                 await Task.Delay(TimeSpan.FromSeconds(Constants.TaskPollingSeconds));
-                return await WrapGrainCall(func, param, timesToRetry);
             }
+            
+            ThisLogger.Error("Too many failed attempts at invoking a grain.");
+            throw new OrleansGrainFailedAfterRetries();
         }
 
         /// <summary>
-        /// Wrap a grain call that returns a <see cref="TResult"/> and takes two parameters.
+        /// Wrap a grain call that takes two parameters.
         /// </summary>
         /// <typeparam name="T1">The first parameter type.</typeparam>
         /// <typeparam name="T2">The second parameter type.</typeparam>
@@ -111,31 +134,38 @@ namespace NIST.CVP.Crypto.Oracle.Helpers
         /// <param name="func">The function to invoke.</param>
         /// <param name="param1">The first parameter to use with the function invoke.</param>
         /// <param name="param2">The second parameter to use with the function invoke.</param>
-        /// <param name="timesToRetry">The number of times to retry the grain call's that failed due to LoadShedding</param>
+        /// <param name="timesToRetry">The number of times to retry the grain calls that failed due to LoadShedding or Timeout.</param>
         /// <returns>Task wrapped TResult</returns>
-        public static async Task<TResult> WrapGrainCall<T1, T2, TResult>(Func<T1, T2, Task<TResult>> func, T1 param1, T2 param2, int timesToRetry)
+        /// <exception cref="OrleansGrainFailedAfterRetries">Thrown when too many failed attempts of invoking grains occurs.</exception>
+        public static async Task WrapGrainCall<T1, T2, TResult>(Func<T1, T2, Task<TResult>> func, T1 param1, T2 param2, int timesToRetry)
         {
-            if (timesToRetry < 0)
+            while (timesToRetry > 0)
             {
-                throw new OrleansGrainFailedAfterRetries();
-            }
-
-            timesToRetry--;
-
-            try
-            {
-                return await func(param1, param2);
-            }
-            catch (GatewayTooBusyException)
-            {
-                // Gateway too busy - wait 5 seconds and try again.
+                timesToRetry--;
+                
+                try
+                {
+                    await func(param1, param2);
+                    return;
+                }
+                catch (TimeoutException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(TimeoutException)} stack: {ex.StackTrace}");
+                }
+                catch (GatewayTooBusyException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(GatewayTooBusyException)} stack: {ex.StackTrace}");
+                }
+                
                 await Task.Delay(TimeSpan.FromSeconds(Constants.TaskPollingSeconds));
-                return await WrapGrainCall(func, param1, param2, timesToRetry);
             }
+            
+            ThisLogger.Error("Too many failed attempts at invoking a grain.");
+            throw new OrleansGrainFailedAfterRetries();
         }
 
         /// <summary>
-        /// Wrap a grain call that returns a <see cref="TResult"/> and takes three parameters.
+        /// Wrap a grain call that takes three parameters.
         /// </summary>
         /// <typeparam name="T1">The first parameter type.</typeparam>
         /// <typeparam name="T2">The second parameter type.</typeparam>
@@ -145,27 +175,34 @@ namespace NIST.CVP.Crypto.Oracle.Helpers
         /// <param name="param1">The first parameter to use with the function invoke.</param>
         /// <param name="param2">The second parameter to use with the function invoke.</param>
         /// <param name="param3">The third parameter to use with the function invoke.</param>
-        /// <param name="timesToRetry">The number of times to retry the grain call's that failed due to LoadShedding</param>
+        /// <param name="timesToRetry">The number of times to retry the grain calls that failed due to LoadShedding or Timeout.</param>
         /// <returns>Task wrapped TResult</returns>
-        public static async Task<TResult> WrapGrainCall<T1, T2, T3, TResult>(Func<T1, T2, T3, Task<TResult>> func, T1 param1, T2 param2, T3 param3, int timesToRetry)
+        /// <exception cref="OrleansGrainFailedAfterRetries">Thrown when too many failed attempts of invoking grains occurs.</exception>
+        public static async Task WrapGrainCall<T1, T2, T3, TResult>(Func<T1, T2, T3, Task<TResult>> func, T1 param1, T2 param2, T3 param3, int timesToRetry)
         {
-            if (timesToRetry < 0)
+            while (timesToRetry > 0)
             {
-                throw new OrleansGrainFailedAfterRetries();
-            }
-
-            timesToRetry--;
-
-            try
-            {
-                return await func(param1, param2, param3);
-            }
-            catch (GatewayTooBusyException)
-            {
-                // Gateway too busy - wait 5 seconds and try again.
+                timesToRetry--;
+                
+                try
+                {
+                    await func(param1, param2, param3);
+                    return;
+                }
+                catch (TimeoutException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(TimeoutException)} stack: {ex.StackTrace}");
+                }
+                catch (GatewayTooBusyException ex)
+                {
+                    ThisLogger.Warn(ex, $"{nameof(GatewayTooBusyException)} stack: {ex.StackTrace}");
+                }
+                
                 await Task.Delay(TimeSpan.FromSeconds(Constants.TaskPollingSeconds));
-                return await WrapGrainCall(func, param1, param2, param3, timesToRetry);
             }
+            
+            ThisLogger.Error("Too many failed attempts at invoking a grain.");
+            throw new OrleansGrainFailedAfterRetries();
         }
     }
 }
