05387063f24f122c20870686d4ae79ae8ba053e5
Chris Celi <christopher.celi@nist.gov>
2020-01-27 16:42:54 -0500

    Adds artifact logging to newmanRun

--- Postman/version/v1.0/newmanRun.js
+++ Postman/version/v1.0/newmanRun.js
@@ -29,19 +29,19 @@ exports.runNewmanTest = function (registration_path, crt_path, key_path, environ
     })
     .on('request', function (err, args) {
         // here, args.response represents the entire response object
-        //var rawBody = args.response.stream, // this is a buffer
-        //     body = rawBody.toString(); // stringified JSON
+        var rawBody = args.response.stream, // this is a buffer
+             body = rawBody.toString(); // stringified JSON
 
-        //results.push(JSON.parse(body)); // this is just to aggregate all responses into one object
+        results.push(JSON.parse(body)); // this is just to aggregate all responses into one object
     })
     // a second argument is also passed to this handler, if more details are needed.
     .on('done', function (err, summary) {
         // write the details to any file of your choice. The format may vary depending on your use case
-        //fs.writeFileSync('totp.json', JSON.stringify(results[0], null, 4));
-        //fs.writeFileSync('accessToken.json', JSON.stringify(results[1], null, 4));
-        //fs.writeFileSync('registration-response.json', JSON.stringify(results[2], null, 4));
+        fs.writeFileSync('totp.json', JSON.stringify(results[0], null, 4));
+        fs.writeFileSync('accessToken.json', JSON.stringify(results[1], null, 4));
+        fs.writeFileSync('registration-response.json', JSON.stringify(results[2], null, 4));
 
-        //fs.writeFileSync('output.json', JSON.stringify(results.slice(3), null, 4));
+        fs.writeFileSync('output.json', JSON.stringify(results.slice(3), null, 4));
 
         if (err || summary.error || summary.run.failures.length > 0) {
             // do something on error
