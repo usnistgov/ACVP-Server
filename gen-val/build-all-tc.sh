1784b78b1c8a69bc73880f019f1a553e29f6bba7
Jason Arnold <jason.arnold@nist.gov>
2020-01-28 14:31:41 -0500

    added initial version of build all genvals shell script converted from bat build file

---
+++ gen-val/build-all-tc.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# set zipcmd var to point to unzip executable
+zipcmd = $(/usr/bin/zip)
+
+# set datetimestamp format YYYYMMDD_HHMM
+datetimestamp = $(date +%Y%m%d_%H%m)
+echo $datetimestamp
+
+# set homedir var from TC %homedir%
+homedir = $(%homedir%)
+echo $homedir
+
+# set netcorappver var from TC %netcorepath%
+netcoreappver = $(%netcoreappver%)
+echo $netcoreappver
+
+cd $homedir
+
+cd gen-val/src/generation/src/NIST.CVP.Generation.GenValApp
+dotnet clean
+dotnet restore
+dotnet build -c Release
+dotnet publish -c Release -r win-x64
+
+cd bin/Release/$netcorappver/win-x64
+$zipcmd -r publish.zip publish/
+mv publish.zip "$homedir/$datetimestamp_GenValsOrleans.zip"
+
+cd $homedir
+
+cd gen-val/src/orleans/src/NIST.CVP.Orleans.ServerHost
+dotnet clean
+dotnet restore
+dotnet build -c Release
+dotnet publish -c Release -r win-x64
+
+cd bin/Release/$netcorappver/win-x64
+$zipcmd -r publish.zip publish/
+mv publish.zip "$homedir/$datetimestamp_OrleansServer.zip"
+
+cd $homedir
+
+cd gen-val/src/pool-api/NIST.CVP.PoolAPI
+dotnet clean
+dotnet restore
+dotnet build -c Release
+dotnet publish -c Release -r win-x64
+
+cd bin/Release/$netcoreappver/win-x64/
+$zipcmd -r publish.zip publish/
+mv publish.zip "$homedir/$datetimestamp_PoolService.zip"
+
+cd $homedir
+
+ls -l *.zip
