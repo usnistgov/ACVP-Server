53a2b2ddf8515b7401d510ab86fd9458c9d6aa4e
Jason Arnold <jason.arnold@nist.gov>
2020-01-28 18:55:58 -0500

    properly wrap variable names throughout script (was sloppy before)

--- gen-val/build-all-tc.sh
+++ gen-val/build-all-tc.sh
@@ -2,17 +2,17 @@
 
 # set datetimestamp format YYYYMMDD_HHMM
 datetimestamp=$(date +%Y%m%d_%H%m)
-echo datetimestamp is $datetimestamp
+echo datetimestamp is ${datetimestamp}
 
 # set homedir var from TC %homedir%
 homedir=$(pwd)
-echo homedir is $homedir
+echo homedir is ${homedir}
 
 # set netcorappver var from TC %netcorepath%
 netcoreappver="netcoreapp3.1"
-echo netcoreappver is $netcoreappver
+echo netcoreappver is ${netcoreappver}
 
-cd $homedir
+cd ${homedir}
 
 cd gen-val/src/generation/src/NIST.CVP.Generation.GenValApp
 dotnet clean
@@ -20,11 +20,11 @@ dotnet restore
 dotnet build -c Release
 dotnet publish -c Release -r win-x64
 
-cd bin/Release/$netcoreappver/win-x64
-/usr/bin/zip -v -r publish.zip publish/
-cp -v ./publish.zip $homedir/$datetimestamp_GenValsOrleans.zip
+cd bin/Release/${netcoreappver}/win-x64
+/usr/bin/zip -q -r publish.zip publish/
+cp -v ./publish.zip ${homedir}/${datetimestamp}_GenValsOrleans.zip
 
-cd $homedir
+cd ${homedir}
 
 cd gen-val/src/orleans/src/NIST.CVP.Orleans.ServerHost
 dotnet clean
@@ -32,11 +32,11 @@ dotnet restore
 dotnet build -c Release
 dotnet publish -c Release -r win-x64
 
-cd bin/Release/$netcoreappver/win-x64
-/usr/bin/zip -v -r publish.zip publish/
-cp -v ./publish.zip $homedir/$datetimestamp_OrleansServer.zip
+cd bin/Release/${netcoreappver}/win-x64
+/usr/bin/zip -q -r publish.zip publish/
+cp -v ./publish.zip ${homedir}/${datetimestamp}_OrleansServer.zip
 
-cd $homedir
+cd ${homedir}
 
 cd gen-val/src/pool-api/NIST.CVP.PoolAPI
 dotnet clean
@@ -44,9 +44,9 @@ dotnet restore
 dotnet build -c Release
 dotnet publish -c Release -r win-x64
 
-cd bin/Release/$netcoreappver/win-x64/
-/usr/bin/zip -v -r publish.zip publish/
-cp -v ./publish.zip $homedir/$datetimestamp_PoolService.zip
+cd bin/Release/${netcoreappver}/win-x64/
+/usr/bin/zip -q -r publish.zip publish/
+cp -v ./publish.zip ${homedir}/${datetimestamp}_PoolService.zip
 
-cd $homedir
+cd ${homedir}
 ls -l *.zip
