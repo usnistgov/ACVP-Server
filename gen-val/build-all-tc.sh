85a0d0c26d71861f67900f412f2d412d34d4d1e9
Jason Arnold <jason.arnold@nist.gov>
2020-01-28 18:28:56 -0500

    add additional debugging options to zip because zip file keeps disappearing mysteriously

--- gen-val/build-all-tc.sh
+++ gen-val/build-all-tc.sh
@@ -2,27 +2,26 @@
 
 # set datetimestamp format YYYYMMDD_HHMM
 datetimestamp=$(date +%Y%m%d_%H%m)
-echo $datetimestamp
+echo datetimestamp is $datetimestamp
 
 # set homedir var from TC %homedir%
 homedir=$(pwd)
-echo $homedir
+echo homedir is $homedir
 
 # set netcorappver var from TC %netcorepath%
 netcoreappver="netcoreapp3.1"
-echo $netcoreappver
+echo netcoreappver is $netcoreappver
 
 cd $homedir
 
 cd gen-val/src/generation/src/NIST.CVP.Generation.GenValApp
-pwd
 dotnet clean
 dotnet restore
 dotnet build -c Release
 dotnet publish -c Release -r win-x64
 
 cd bin/Release/$netcoreappver/win-x64
-/usr/bin/zip -r $homedir/$datetimestamp_GenValsOrleans.zip publish/
+/usr/bin/zip -v -r $homedir/$datetimestamp_GenValsOrleans.zip publish/
 #mv publish.zip "$homedir/$datetimestamp_GenValsOrleans.zip"
 
 cd $homedir
@@ -34,7 +33,7 @@ dotnet build -c Release
 dotnet publish -c Release -r win-x64
 
 cd bin/Release/$netcoreappver/win-x64
-/usr/bin/zip -r $homedir/$datetimestamp_OrleansServer.zip publish/
+/usr/bin/zip -v -r $homedir/$datetimestamp_OrleansServer.zip publish/
 #mv publish.zip "$homedir/$datetimestamp_OrleansServer.zip"
 
 cd $homedir
@@ -46,7 +45,7 @@ dotnet build -c Release
 dotnet publish -c Release -r win-x64
 
 cd bin/Release/$netcoreappver/win-x64/
-/usr/bin/zip -r $homedir/$datetimestamp_PoolService.zip publish/
+/usr/bin/zip -v -r $homedir/$datetimestamp_PoolService.zip publish/
 #mv publish.zip "$homedir/$datetimestamp_PoolService.zip"
 
 cd $homedir
