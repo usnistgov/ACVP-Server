1fffb9cdf9bb38fd3a6c08ca148fb33cfdab986c
Chris Celi <christopher.celi@nist.gov>
2020-01-30 11:57:19 -0500

    Removes old Stored Procedure references in code

--- TaskQueueProcessor/src/NIST.CVP.TaskQueueProcessor/Constants/StoredProcedures.cs
+++ TaskQueueProcessor/src/NIST.CVP.TaskQueueProcessor/Constants/StoredProcedures.cs
@@ -6,12 +6,6 @@ namespace NIST.CVP.TaskQueueProcessor.Constants
         public const string DELETE_TASK_FROM_TASK_QUEUE = "common.TaskQueueDelete";
         public const string UPDATE_IN_PROGRESS_TASK_TO_READY = "common.TaskQueueRestart";
         public const string GET_JSON = "acvp.VectorSetJsonGet";
-        public const string PUT_JSON = "acvp.VectorSetJsonPut";
-        
-        public const string GET_CAPABILITIES = "acvp.CapabilitiesGet";
-        public const string GET_SUBMITTED = "acvp.SubmittedGet";
-        public const string PUT_ALL_PROMPT_DATA = "acvp.PromptDataPut";
-        public const string PUT_VALIDATION = "acvp.ValidationPut";
-        public const string PUT_ERROR = "acvp.ErrorPut";
+        public const string PUT_JSON = "acvp.VectorSetJsonPut"; 
     }
 }
