930e242acb15b97527e16e2cd467775bb9a86d1b
Hammett <rlh4@NIST.GOV>
2020-02-03 10:26:09 -0500

    Updates pool instantiation to grab initial counts from singleton - avoids multiple queries (one per pool) to get initial pool counts. - fixes #150

---
+++ DB/ACVPPools_DB/dbo/Stored Procedures/GetPoolCounts.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Russ Hammett
+-- Create date: 2020-02-03
+-- Description:	Get all pool counts for initial app bootstrapping.
+-- =============================================
+CREATE PROCEDURE [dbo].[GetPoolCounts]
+	
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+	SELECT	poolId, poolName, poolCount
+	FROM	(
+		SELECT	poolId, COUNT_BIG(1) as poolCount
+		FROM	[dbo].[PoolValues] pv
+		WHERE	pv.isStagingValue = 0
+		GROUP	BY pv.poolId
+	) poolCounts
+	INNER	JOIN [dbo].[PoolInformation] poolInfo on poolCounts.poolId = poolInfo.id
+
+END
