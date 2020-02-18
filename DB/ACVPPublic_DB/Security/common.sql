a85a3d99304d4dd528fd65447c564ca27e6a5c0b
Chris Celi <christopher.celi@nist.gov>
2020-02-18 12:18:01 -0500

    Adds Stored Procs and Table for user auth

--- DB/ACVPPublic_DB/Security/common.sql
+++ DB/ACVPPublic_DB/Security/common.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [common]
     AUTHORIZATION [dbo];
 
+
+
---
+++ DB/ACVPPublic_DB/acvp/Stored Procedures/PreviousComputedWindowByUserSet.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [acvp].[PreviousComputedWindowByUserSet]
+    @CertificateRawData VARBINARY(MAX),
+    @LastUsedWindow BIGINT
+	
+AS
+
+DECLARE @UserId BIGINT
+
+SELECT TOP(1) @UserId = id
+FROM [acvp].[ACVP_USER] au
+WHERE au.certificate = @CertificateRawData
+
+UPDATE [acvp].[AcvpUserAuthentications]
+SET LastUsedWindow = @LastUsedWindow
+WHERE AcvpUserID = @UserId
+
+IF @@ROWCOUNT = 0
+    INSERT INTO [acvp].[AcvpUserAuthentications] (AcvpUserId, LastUsedWindow)
+    VALUES (@UserId, @LastUsedWindow)
