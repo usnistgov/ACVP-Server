8da6c29274ef622b124a241eaeeb82e00e3ce4a4
sds1 <shane.shaffer@nist.gov>
2020-02-26 17:24:47 -0500

    Removed sone Console.Writes that didn't belong (I didn't put them there...)

--- ACVPCore/Services/ImplementationService.cs
+++ ACVPCore/Services/ImplementationService.cs
@@ -73,9 +73,8 @@ namespace ACVPCore.Services
 			//Update the implementation record if needed.
 			if (parameters.NameUpdated || parameters.DescriptionUpdated || parameters.TypeUpdated || parameters.VersionUpdated || parameters.WebsiteUpdated || parameters.OrganizationIDUpdated || parameters.AddressIDUpdated)
 			{
-				Console.WriteLine("Inside the service-level check for changes");
 				Result implementationUpdateResult = _implementationProvider.Update(parameters.ID, parameters.Name, parameters.Description, parameters.Type, parameters.Version, parameters.Website, parameters.OrganizationID, parameters.AddressID, parameters.NameUpdated, parameters.DescriptionUpdated, parameters.TypeUpdated, parameters.VersionUpdated, parameters.WebsiteUpdated, parameters.OrganizationIDUpdated, parameters.AddressIDUpdated);
-				Console.WriteLine("Inside the service-level check for update success");
+
 				if (!implementationUpdateResult.IsSuccess)
 				{
 					return new ImplementationResult(implementationUpdateResult.ErrorMessage);
