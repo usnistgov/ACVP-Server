67458d1340f673b808c809cbae77124e8a877361
sds1 <shane.shaffer@nist.gov>
2020-03-03 09:13:32 -0500

    Make the workflow item payload be the original JSON, not the serialized payload object, which may contain extra data

--- ACVPWorkflow/Services/IWorkflowService.cs
+++ ACVPWorkflow/Services/IWorkflowService.cs
@@ -8,7 +8,7 @@ namespace ACVPWorkflow.Services
 {
 	public interface IWorkflowService
 	{
-		WorkflowInsertResult AddWorkflowItem(APIAction apiAction, long requestID, IWorkflowItemPayload payload, long userID);
+		WorkflowInsertResult AddWorkflowItem(APIAction apiAction, long requestID, string payload, long userID);
 		Result Validate(WorkflowItem workflowItem);
 		Result Approve(WorkflowItem workflowItem);
 		Result Reject(WorkflowItem workflowItem);
