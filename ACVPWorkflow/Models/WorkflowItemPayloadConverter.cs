a32182c55402983747ef5d1f1c35e1467d7e2cb5
sds1 <shane.shaffer@nist.gov>
2020-03-03 11:04:22 -0500

    Added a JsonConverter to help with the serialization of the IWorkflowItemPayload in WorkflowItem - left the deserializer unimplemented because we don't need it

---
+++ ACVPWorkflow/Models/WorkflowItemPayloadConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ACVPWorkflow.Models
+{
+	public class WorkflowItemPayloadConverter : JsonConverter<IWorkflowItemPayload>
+	{
+		public override IWorkflowItemPayload Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Write(Utf8JsonWriter writer, IWorkflowItemPayload value, JsonSerializerOptions options)
+		{
+			switch (value)
+			{
+				case CertifyTestSessionPayload p: JsonSerializer.Serialize(writer, p, typeof(CertifyTestSessionPayload), options); break;
+				case DeletePayload p: JsonSerializer.Serialize(writer, p, typeof(DeletePayload), options); break;
+				case DependencyCreatePayload p: JsonSerializer.Serialize(writer, p, typeof(DependencyCreatePayload), options); break;
+				case DependencyUpdatePayload p: JsonSerializer.Serialize(writer, p, typeof(DependencyUpdatePayload), options); break;
+				case ImplementationCreatePayload p: JsonSerializer.Serialize(writer, p, typeof(ImplementationCreatePayload), options); break;
+				case ImplementationUpdatePayload p: JsonSerializer.Serialize(writer, p, typeof(ImplementationUpdatePayload), options); break;
+				case OECreatePayload p: JsonSerializer.Serialize(writer, p, typeof(OECreatePayload), options); break;
+				case OEUpdatePayload p: JsonSerializer.Serialize(writer, p, typeof(OEUpdatePayload), options); break;
+				case OrganizationCreatePayload p: JsonSerializer.Serialize(writer, p, typeof(OrganizationCreatePayload), options); break;
+				case OrganizationUpdatePayload p: JsonSerializer.Serialize(writer, p, typeof(OrganizationUpdatePayload), options); break;
+				case PersonCreatePayload p: JsonSerializer.Serialize(writer, p, typeof(PersonCreatePayload), options); break;
+				case PersonUpdatePayload p: JsonSerializer.Serialize(writer, p, typeof(PersonUpdatePayload), options); break;
+			}
+		}
+	}
+}
