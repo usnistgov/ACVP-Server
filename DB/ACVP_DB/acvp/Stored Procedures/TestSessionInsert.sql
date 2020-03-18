92124494b30c159d9970ac370f0f41c2052fc4b3
sds1 <shane.shaffer@nist.gov>
2020-03-17 22:25:13 -0400

    Populate the Test Session status when it is created...

--- DB/ACVP_DB/acvp/Stored Procedures/TestSessionInsert.sql
+++ DB/ACVP_DB/acvp/Stored Procedures/TestSessionInsert.sql
@@ -11,5 +11,5 @@ AS
 
 SET NOCOUNT ON
 
-INSERT INTO acvp.TEST_SESSION (id, acv_version_id, generator, [sample], publishable, [user_id], created_on)
-VALUES (@TestSessionID, @ACVVersionID, @Generator, @IsSample, @Publishable, @UserID, CURRENT_TIMESTAMP)
+INSERT INTO acvp.TEST_SESSION (id, acv_version_id, generator, [sample], publishable, TestSessionStatusId, [user_id], created_on)
+VALUES (@TestSessionID, @ACVVersionID, @Generator, @IsSample, @Publishable, 2, @UserID, CURRENT_TIMESTAMP)
