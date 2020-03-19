c9d2083780f8c031eb2a4d6c26e4aabe88847e05
Robert Staples <robert.staples@nist.gov>
2020-03-19 13:45:41 -0400

    Support for Deleting Dependencies
    Support for Deleting Dependencies

--- Web.Admin/ClientApp/src/app/components/workflow/dependency/delete/workflow-dependency-delete.component.ts
+++ Web.Admin/ClientApp/src/app/components/workflow/dependency/delete/workflow-dependency-delete.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AjaxService } from '../../../../services/ajax/ajax.service';
 import { Router } from '@angular/router';
 import { WorkflowItemBase } from '../../../../models/Workflow/WorkflowItemBase';
+import { WorkflowDeletePayload } from '../../../../models/Workflow/WorkflowDeletePayload';
+import { Dependency } from '../../../../models/dependency/dependency';
 
 @Component({
   selector: 'app-workflow-dependency-delete',
@@ -10,41 +12,48 @@ import { WorkflowItemBase } from '../../../../models/Workflow/WorkflowItemBase';
 })
 export class WorkflowDependencyDeleteComponent implements OnInit {
 
+  currentState: Dependency;
+  workflowItem: WorkflowItemBase<WorkflowDeletePayload>;
+  objectKeys = Object.keys;
+
   constructor(private ajs: AjaxService, private router: Router) { }
 
-  //approveWorkflow() {
-  //  this.ajs.approveWorkflow(this.workflowItem.workflowItemID).subscribe(
-  //    data => { this.refreshPageData(); },
-  //    err => { },
-  //    () => { }
-  //  );
-  //}
-  //rejectWorkflow() {
-  //  this.ajs.rejectWorkflow(this.workflowItem.workflowItemID).subscribe(
-  //    data => { this.refreshPageData(); },
-  //    err => { },
-  //    () => { }
-  //  );
-  //}
+  approveWorkflow() {
+    this.ajs.approveWorkflow(this.workflowItem.workflowItemID).subscribe(
+      data => { this.refreshPageData(); },
+      err => { },
+      () => { }
+    );
+  }
+  rejectWorkflow() {
+    this.ajs.rejectWorkflow(this.workflowItem.workflowItemID).subscribe(
+      data => { this.refreshPageData(); },
+      err => { },
+      () => { }
+    );
+  }
 
-  //refreshPageData() {
-  //  this.router.navigateByUrl('/', { skipLocationChange: true })
-  //    .then(() =>
-  //      this.router.navigate(['workflow/' + this.workflowItem.workflowItemID])
-  //    );
-  //}
-
-  //workflowItem: WorkflowItemBase<WorkflowDependencyDeletePayload>;
-  objectKeys = Object.keys;
+  refreshPageData() {
+    this.router.navigateByUrl('/', { skipLocationChange: true })
+      .then(() =>
+        this.router.navigate(['workflow/' + this.workflowItem.workflowItemID])
+      );
+  }
 
   /*
    * This is how the component takes the workflowItem from the main workflow controller using the
    * [wfItem]="workflowItem" syntax in the HTML template.  i.e. how custom element attributes are specified
    */
-  //@Input()
-  //set wfItem(item: WorkflowItemBase<WorkflowDependencyDeletePayload>) {
-  //  this.workflowItem = item;
-  //}
+  @Input()
+  set wfItem(item: WorkflowItemBase<WorkflowDeletePayload>) {
+    this.workflowItem = item;
+
+    this.ajs.getDependency(this.workflowItem.payload.id).subscribe(
+      data => { this.currentState = data; console.log(this.currentState); },
+      err => { },
+      () => { }
+    );
+  }
 
   ngOnInit() {
   }
