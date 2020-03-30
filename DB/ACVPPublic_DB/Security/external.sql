90bbe8adcd4f65fb3c7c1d8a5cbc8878f56e303c
Chris Celi <christopher.celi@nist.gov>
2020-03-30 17:29:50 -0400

    Adds SPs to source finally

--- DB/ACVPPublic_DB/Security/external.sql
+++ DB/ACVPPublic_DB/Security/external.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [external]
     AUTHORIZATION [dbo];
 
+
+
---
+++ DB/ACVPPublic_DB/acvp/Stored Procedures/AcvpUserSeedGetByCertificate.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [acvp].[AcvpUserSeedGetByCertificate]
+    @CertificateRawData VARBINARY(MAX)
+	
+AS
+
+SELECT (seed)
+FROM [acvp].[ACVP_USER] au
+WHERE au.certificate = @CertificateRawData
