76b2c80ef1cb929dac3bdfd0636bb7aa242866d4
Chris Celi <christopher.celi@nist.gov>
2020-04-23 16:35:39 -0400

    Stubs out some conditions, but need discussion

--- Web.Public/src/Services/WorkflowItemPayloadValidators/CancelTestSessionPayloadValidator.cs
+++ Web.Public/src/Services/WorkflowItemPayloadValidators/CancelTestSessionPayloadValidator.cs
@@ -9,7 +9,8 @@ namespace Web.Public.Services.WorkflowItemPayloadValidators
 		public PayloadValidationResult Validate(IWorkflowItemPayload workflowItemPayload)
 		{
 			// TODO can you cancel something that has already been published?
-			
+			var payload = (CancelPayload) workflowItemPayload;
+
 			// Check that the test session exists is done by JWT claims
 			return new PayloadValidationResult(new List<string>());
 		}
--- Web.Public/src/Services/WorkflowItemPayloadValidators/CancelVectorSetPayloadValidator.cs
+++ Web.Public/src/Services/WorkflowItemPayloadValidators/CancelVectorSetPayloadValidator.cs
@@ -9,7 +9,8 @@ namespace Web.Public.Services.WorkflowItemPayloadValidators
 		public PayloadValidationResult Validate(IWorkflowItemPayload workflowItemPayload)
 		{
 			// TODO can you cancel something that has already been published?
-			
+			var payload = (CancelPayload) workflowItemPayload;
+
 			// Check that the vector set exists and is an element of the test session is done by JWT claims
 			return new PayloadValidationResult(new List<string>());
 		}
