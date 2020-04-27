c9cb05d6a2f9d187de15c98804cdace2bdf7125a
Russell Hammett <Kritner@gmail.com>
2020-04-27 14:53:32 -0400

    Updates for public db from Chris

---
+++ DB/ACVPPublic_DB/acvp/Stored Procedures/RequestGetFromUser.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [acvp].[RequestGetFromUser]
+	@UserID bigint
+AS
+
+SET NOCOUNT ON
+
+SELECT id AS ID
+    ,APIActionID AS APIActionID
+    ,[Status] AS [Status]
+    ,AcceptID AS AcceptID
+FROM [acvp].[Request]
+WHERE UserID = @UserID
+ORDER BY CreatedOn
---
+++ DB/ACVPPublic_DB/val/Stored Procedures/DependenciesForOEGet.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [val].[DependenciesForOEGet]
+    @OEID BIGINT
+	
+AS
+
+SELECT dependency_id AS DependencyID
+FROM [val].[VALIDATION_OE_DEPENDENCY_LINK]
+WHERE validation_oe_id = @OEID
