fd63b09f92f20bb48a0eb57fc0c37c15a8760cf4
Staples <rls4@NIST.GOV>
2020-05-13 17:37:06 -0400

    Quick change to make the dependency create page display a statement when no custom attributes are defined.  Also, refactored the ui to use a custom filter isntead o manually ignoring attributes
    Quick change to make the dependency create page display a statement when no custom attributes are defined.  Also, refactored the ui to use a custom filter isntead o manually ignoring attributes

--- Web.Admin/ClientApp/src/app/components/workflow/dependency/create/workflow-dependency-create.component.ts
+++ Web.Admin/ClientApp/src/app/components/workflow/dependency/create/workflow-dependency-create.component.ts
@@ -12,7 +12,6 @@ export class WorkflowDependencyCreateComponent implements OnInit {
   constructor() { }
 
   workflowItem: WorkflowItemBase<WorkflowDependencyCreatePayload>;
-  objectKeys = Object.keys;
 
   /*
    * This is how the component takes the workflowItem from the main workflow controller using the
@@ -21,6 +20,22 @@ export class WorkflowDependencyCreateComponent implements OnInit {
   @Input()
   set wfItem(item: WorkflowItemBase<WorkflowDependencyCreatePayload>) {
     this.workflowItem = item;
+  }
+
+  // This is simply a utility function used to return a form of the attributes with the non-user-defined ones out, as well
+  // as the ones related to ACVP UI, like id, url, etc.
+  filterKeys() {
+    return Object.keys(this.workflowItem.payload).filter(function (element, index, array) {
+      if (element === "id" ||
+        element === "name" ||
+        element === "type" ||
+        element === "description" ||
+        element === "url") {
+        return false;
+      } else {
+        return true;
+      }
+    });
   }
 
   ngOnInit() {
