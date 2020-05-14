8b9fe993aa8a3fa669cddecb7b46b1b596447769
sds1 <shane.shaffer@nist.gov>
2020-05-14 13:27:54 -0400

    Killed more tables

--- DB/ACVP_DB/val/Tables/ValidationApprovalHistory.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [val].[ValidationApprovalHistory]
-(
-	[ValidationApprovalHistoryId] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	[ValidationId] BIGINT NOT NULL, 
-    [TestSessionId] BIGINT NOT NULL, 
-    [OperatingEnvironmentId] BIGINT NOT NULL, 
-    [ApprovedDate] DATETIME2 NOT NULL
-)
