534123db91d165d4ba74705d9314f7d1757a1ee2
Staples <rls4@NIST.GOV>
2020-05-14 23:06:48 -0400

    Added link from TQ to relevant TS/VS for Chris
    Added link from TQ to relevant TS/VS for Chris

--- Web.Admin/ClientApp/src/app/components/taskQueue/task-queue.component.ts
+++ Web.Admin/ClientApp/src/app/components/taskQueue/task-queue.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskQueue } from '../../models/taskQueue/TaskQueue';
 import { AdministrativeAjaxProviderService } from '../../services/ajax/administrative/administrative-ajax-provider.service';
+import { TestSessionProviderService } from '../../services/ajax/testSession/test-session-provider.service';
+import { TestSessionListParameters } from '../../models/testSession/TestSessionListParameters';
 
 @Component({
   selector: 'app-task-queue',
@@ -11,7 +13,8 @@ export class TaskQueueComponent implements OnInit {
 
   taskQueue: TaskQueue;
 
-  constructor(private AdministrativeAjax: AdministrativeAjaxProviderService) { }
+  constructor(private AdministrativeAjax: AdministrativeAjaxProviderService,
+    private TestSessionProvider: TestSessionProviderService) { }
 
   deleteTask(id: number) {
     this.AdministrativeAjax.deleteTask(id).subscribe(
@@ -22,10 +25,29 @@ export class TaskQueueComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    let self = this;
+
     this.AdministrativeAjax.getTaskQueue().subscribe(
       data => {
         this.taskQueue = data;
+
+        this.taskQueue.data.forEach(function (task, index, array) {
+          let sessionSearchParams = new TestSessionListParameters("", "", "");
+          sessionSearchParams.VectorSetId = task.vectorSetID.toString();
+
+          self.TestSessionProvider.getTestSessions(sessionSearchParams).subscribe(
+            data => {
+              if (data.data.length > 0) {
+                // This works because if a TS exists, there should only ever be one
+                task.testSessionId = data.data[0].testSessionId;
+              }
+            }
+          );
+
+        });
+
       });
-  };
+  }
 
 }
