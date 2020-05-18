8e443bee528b6624bfdbc84ec34a263bed443070
Russell Hammett <Kritner@gmail.com>
2020-05-18 14:29:06 -0400

    TupleHash NRE fix

--- gen-val/src/generation/src/NIST.CVP.Generation/TupleHash/v1_0/ParameterValidator.cs
+++ gen-val/src/generation/src/NIST.CVP.Generation/TupleHash/v1_0/ParameterValidator.cs
@@ -66,6 +66,12 @@ namespace NIST.CVP.Generation.TupleHash.v1_0
                 errorResults.Add(result);
             }
 
+            if (parameters.XOF == null)
+            {
+                errorResults.Add("XOF must be provided.");
+                return;
+            }
+            
             if ((parameters.XOF.Length != 1 && parameters.XOF.Length != 2) || parameters.XOF.ToHashSet().Count != parameters.XOF.Length)
             {
                 errorResults.Add("XOF must contain only a single true, a single false, or both");
