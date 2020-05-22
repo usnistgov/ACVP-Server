99e72c78cec69921341506767d3b98218bfa0901
Russell Hammett <Kritner@gmail.com>
2020-05-22 15:47:43 -0400

    Removes the requirement of passing in "algorithm" and "revision" from the POST/PUT of client responses

--- Web.Public/src/Services/MessagePayloadValidators/ResubmitVectorSetResultsPayloadValidator.cs
+++ Web.Public/src/Services/MessagePayloadValidators/ResubmitVectorSetResultsPayloadValidator.cs
@@ -20,16 +20,6 @@ namespace Web.Public.Services.MessagePayloadValidators
 			var payload = (VectorSetSubmissionPayload) workflowItemPayload;
 			var errors = new List<string>();
 
-			if (string.IsNullOrWhiteSpace(payload.Algorithm))
-			{
-				errors.Add("algorithm not provided");
-			}
-
-			if (string.IsNullOrWhiteSpace(payload.Revision))
-			{
-				errors.Add("revision not provided");
-			}
-
 			//Vector set must be in Failed status
 			if (_vectorSetService.GetStatus(payload.VectorSetID) != VectorSetStatus.Failed)
 			{
