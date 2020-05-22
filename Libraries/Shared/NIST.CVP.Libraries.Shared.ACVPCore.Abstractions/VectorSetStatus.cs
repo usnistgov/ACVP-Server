8d7c79e32f3e382039f8aacee72c4142251520e9
Russell Hammett <Kritner@gmail.com>
2020-05-22 17:10:03 -0400

    Update casing on status returned from `{tsId}/results`

--- Libraries/Shared/NIST.CVP.Libraries.Shared.ACVPCore.Abstractions/VectorSetStatus.cs
+++ Libraries/Shared/NIST.CVP.Libraries.Shared.ACVPCore.Abstractions/VectorSetStatus.cs
@@ -1,14 +1,24 @@
-﻿namespace NIST.CVP.Libraries.Shared.ACVPCore.Abstractions
+﻿using System.Runtime.Serialization;
+
+namespace NIST.CVP.Libraries.Shared.ACVPCore.Abstractions
 {
 	public enum VectorSetStatus
 	{
+		[EnumMember(Value = "initial")]
 		Initial = 0,
+		[EnumMember(Value = "processed")]
 		Processed = 1,
+		[EnumMember(Value = "katReceieved")]
 		KATReceived = 2,
+		[EnumMember(Value = "passed")]
 		Passed = 3,
+		[EnumMember(Value = "failed")]
 		Failed = 4,
+		[EnumMember(Value = "cancelled")]
 		Cancelled = 5,
+		[EnumMember(Value = "resubmitAnswers")]
 		ResubmitAnswers = 6,
+		[EnumMember(Value = "error")]
 		Error = -1
 	}
 }
