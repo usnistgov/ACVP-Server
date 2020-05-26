cffb3e5ef395c795441574c884ea34d7b6f64d5a
Russell Hammett <Kritner@gmail.com>
2020-05-26 08:21:32 -0400

    Updates "algorithm" -> "name" for algorithms endpoint

--- Libraries/Shared/NIST.CVP.Libraries.Shared.Algorithms.External/AlgorithmBase.cs
+++ Libraries/Shared/NIST.CVP.Libraries.Shared.Algorithms.External/AlgorithmBase.cs
@@ -8,7 +8,7 @@ namespace NIST.CVP.Libraries.Shared.Algorithms.External
 		[JsonPropertyName("id")]
 		public int AlgorithmId { get; set; }
 		
-		[JsonPropertyName("algorithm")]
+		[JsonPropertyName("name")]
 		public string Name { get; set; }
 
 		[JsonPropertyName("mode")]
