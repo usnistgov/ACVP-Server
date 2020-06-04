e39d78379888a0dbaa638854f0fcca7caacf1b8d
sds1 <shane.shaffer@nist.gov>
2020-06-04 18:40:23 -0400

    Added some exception handling to the message queue provider, just because it should do it

--- Libraries/Internal/NIST.CVP.Libraries.Internal.MessageQueue/Providers/MessageQueueProvider.cs
+++ Libraries/Internal/NIST.CVP.Libraries.Internal.MessageQueue/Providers/MessageQueueProvider.cs
@@ -1,62 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using NIST.CVP.Libraries.Shared.DatabaseInterface;
+using Microsoft.Extensions.Logging;
 using Mighty;
+using NIST.CVP.Libraries.Shared.DatabaseInterface;
+using NIST.CVP.Libraries.Shared.ExtensionMethods;
 
 namespace NIST.CVP.Libraries.Internal.MessageQueue.Providers
 {
 	public class MessageQueueProvider : IMessageQueueProvider
 	{
 		private readonly string _acvpConnectionString;
+		private readonly ILogger<MessageQueueProvider> _logger;
 
-		public MessageQueueProvider(IConnectionStringFactory connectionStringFactory)
+		public MessageQueueProvider(IConnectionStringFactory connectionStringFactory, ILogger<MessageQueueProvider> logger)
 		{
 			_acvpConnectionString = connectionStringFactory.GetMightyConnectionString("ACVP");
+			_logger = logger;
 		}
 
 		public Message GetNextMessage()
 		{
-			var db = new MightyOrm<Message>(_acvpConnectionString);
+			try
+			{
+				var db = new MightyOrm<Message>(_acvpConnectionString);
 
-			return db.SingleFromProcedure("common.MessageQueueGetNext");
+				return db.SingleFromProcedure("common.MessageQueueGetNext");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex);
+				return null;
+			}
 		}
 
 		public void UpdateStatus(Guid id, MessageStatus messageStatus)
 		{
-			var db = new MightyOrm(_acvpConnectionString);
-
-			db.ExecuteProcedure("common.MessageQueueUpdateStatus", inParams: new
+			try
 			{
-				MessageId = id,
-				StatusId = messageStatus
-			});
+				var db = new MightyOrm(_acvpConnectionString);
+
+				db.ExecuteProcedure("common.MessageQueueUpdateStatus", inParams: new
+				{
+					MessageId = id,
+					StatusId = messageStatus
+				});
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex);
+			}
 		}
 
 		public void DeleteMessage(Guid id)
 		{
-			var db = new MightyOrm(_acvpConnectionString);
-
-			db.ExecuteProcedure("common.MessageQueueDelete", inParams: new
+			try
 			{
-				MessageId = id
-			});
+				var db = new MightyOrm(_acvpConnectionString);
+
+				db.ExecuteProcedure("common.MessageQueueDelete", inParams: new
+				{
+					MessageId = id
+				});
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex);
+			}
 		}
 
 		public List<MessageQueueItem> List()
 		{
-			var db = new MightyOrm<MessageQueueItem>(_acvpConnectionString);
+			try
+			{
+				var db = new MightyOrm<MessageQueueItem>(_acvpConnectionString);
 
-			return db.QueryFromProcedure("common.MessageQueueList").ToList();
+				return db.QueryFromProcedure("common.MessageQueueList").ToList();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex);
+				return new List<MessageQueueItem>();
+			}
 		}
 
 		public string GetMessagePayload(Guid id)
 		{
-			var db = new MightyOrm(_acvpConnectionString);
-			return db.ScalarFromProcedure("common.MessageGetPayload", inParams: new
+			try
 			{
-				MessageId = id
-			}).ToString();
+				var db = new MightyOrm(_acvpConnectionString);
+				return db.ScalarFromProcedure("common.MessageGetPayload", inParams: new
+				{
+					MessageId = id
+				}).ToString();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex);
+				return null;
+			}
 		}
 	}
 }
