a61a18ca6208e7f500f024d8d8a4481469e76bdb
Joachim Vandersmissen <git@jvdsn.com>
2025-03-21 14:29:29 -0500

    Add abstractions for XECDH grains

---
+++ gen-val/src/orleans/src/NIST.CVP.ACVTS.Libraries.Orleans.Grains.Interfaces/Xecdh/IOracleObserverXecdhSscCaseGrain.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using NIST.CVP.ACVTS.Libraries.Oracle.Abstractions.ParameterTypes;
+using NIST.CVP.ACVTS.Libraries.Oracle.Abstractions.ResultTypes;
+using Orleans;
+
+namespace NIST.CVP.ACVTS.Libraries.Orleans.Grains.Interfaces.Xecdh
+{
+    public interface IOracleObserverXecdhSscCaseGrain : IGrainWithGuidKey, IGrainObservable<XecdhSscResult>
+    {
+        Task<bool> BeginWorkAsync(XecdhSscParameters param);
+    }
+}
